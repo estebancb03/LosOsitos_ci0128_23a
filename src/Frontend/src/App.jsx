29089854b5ab4f5e4984c7e3572b04211f16136a
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-18 20:50:10 -0600

    Se crea estructura de rutas y página home en blanco

--- src/Frontend/src/App.jsx
+++ src/Frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div className='bg-green-500'>
-    <h1>Hello world</h1>
-  </div>
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        //Public access routes
+        <Route path="/">
+          <Route index element={<Home />} />
+        </Route>
+        //Private access routes
+        <Route path="/admin">
+          <Route index />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
