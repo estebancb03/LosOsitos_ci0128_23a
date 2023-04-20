61672bfa2d032792b37f80920937fb3e8eb84b66
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-19 19:31:56 -0600

    Se agregan y se corrigen resoluciones de pantalla

--- src/Frontend/tailwind.config.js
+++ src/Frontend/tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     extend: {},
     screens: {
       'sm': {'max': '767px'},
-      // => @media (max-width: 767px) { ... }
+      // => @media (min-width: 640px and max-width: 767px) { ... }
 
       'md': {'min': '768px', 'max': '1023px'},
       // => @media (min-width: 768px and max-width: 1023px) { ... }
