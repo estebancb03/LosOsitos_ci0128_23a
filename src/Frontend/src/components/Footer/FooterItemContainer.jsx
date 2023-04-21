bba5924d6359b2101c9416d96f5e4eef845ab905
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-20 20:59:58 -0600

    Se recorre archivo de datos y por cada elemento se crea un FooterItem

--- src/Frontend/src/components/Footer/FooterItemContainer.jsx
+++ src/Frontend/src/components/Footer/FooterItemContainer.jsx
@@ -1,5 +1,12 @@
-const FooterItemContainer = () => {
-  return <div>FooterItemContainer</div>;
-};
+import FooterItem from "./FooterItem"
+import FooterData from "../data/FooterData"
 
-export default FooterItemContainer;
+const FooterItemsContainer = () => {
+  return <div className="grid grid-cols-3 sm:grid-cols-1 gap-6 sm:px-8 px-5 py-16 text-center">
+    {FooterData.map((item, index) => (
+      <FooterItem item={item} key={index} />
+    ))}
+  </div>
+}
+
+export default FooterItemsContainer
