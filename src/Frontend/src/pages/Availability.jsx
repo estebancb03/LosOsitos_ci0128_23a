2144ba39f1895215d0916f1124dc32c8aa87458a
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-23 14:13:23 -0600

    Se agregan datos de prueba quemados para validar el diseño de la tabla

--- src/Frontend/src/pages/Availability.jsx
+++ src/Frontend/src/pages/Availability.jsx
@@ -1,7 +1,9 @@
 import Title from "../components/Title";
 import Table from "../components/Table";
+import TableItem from "../components/TableItem";
 import Container from "../components/Container";
 import Footer from "../components/Footer/Footer";
+import StatusText from "../components/StatusText";
 import NavMenu from "../components/NavMenu/NavMenu";
 
 const Availability = () => {
@@ -9,8 +11,28 @@ const Availability = () => {
     <NavMenu />
     <Container>
       <Title name="Availability" />
-      <Table>
-        
+      <Table colums={['Parcel', 'Customer', 'Start date', 'End date', 'State', 'Action']}>
+        <TableItem number='1' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Reserved" status="reserved" />, <button>Holaa</button>]}/>
+        <TableItem number='2' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Available" status="available" />, <button>Holaa</button>]}/>
+        <TableItem number='3' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Unvailable" status="unavailable" />, <button>Holaa</button>]}/>
+        <TableItem number='4' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Reserved" status="reserved" />, <button>Holaa</button>]}/>
+        <TableItem number='5' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Available" status="available" />, <button>Holaa</button>]}/>
+        <TableItem number='6' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Unvailable" status="unavailable" />, <button>Holaa</button>]}/>
+        <TableItem number='7' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Reserved" status="reserved" />, <button>Holaa</button>]}/>
+        <TableItem number='8' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Available" status="available" />, <button>Holaa</button>]}/>
+        <TableItem number='9' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Unvailable" status="unavailable" />, <button>Holaa</button>]}/>
+        <TableItem number='10' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Reserved" status="reserved" />, <button>Holaa</button>]}/>
+        <TableItem number='11' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Available" status="available" />, <button>Holaa</button>]}/>
+        <TableItem number='12' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Unvailable" status="unavailable" />, <button>Holaa</button>]}/>
+        <TableItem number='13' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Reserved" status="reserved" />, <button>Holaa</button>]}/>
+        <TableItem number='14' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Available" status="available" />, <button>Holaa</button>]}/>
+        <TableItem number='15' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Unvailable" status="unavailable" />, <button>Holaa</button>]}/>
+        <TableItem number='16' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Reserved" status="reserved" />, <button>Holaa</button>]}/>
+        <TableItem number='17' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Available" status="available" />, <button>Holaa</button>]}/>
+        <TableItem number='18' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Unvailable" status="unavailable" />, <button>Holaa</button>]}/>
+        <TableItem number='19' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Reserved" status="reserved" />, <button>Holaa</button>]}/>
+        <TableItem number='20' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Available" status="available" />, <button>Holaa</button>]}/>
+        <TableItem number='21' data={['1','Esteban', '23/04/2023', '25/04/2023', <StatusText text="Unvailable" status="unavailable" />, <button>Holaa</button>]}/>
       </Table>
     </Container>
     <Footer />
