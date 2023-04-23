758d58b595c80cfe07293c62e02e3a0ab8ec17af
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-23 14:07:43 -0600

    Se agrega clases a los elemntos hios de la tabla y se controla el overflow de la misma

--- src/Frontend/src/components/Table.jsx
+++ src/Frontend/src/components/Table.jsx
@@ -1,20 +1,22 @@
 const Table = ({ colums, children }) => {
   return (
-    <table className="my-5 w-full">
-      <thead className="bg-gray-50 border-b-2 border-gray-200">
-        <tr>
-          {colums.map((colum, index) => (
-            <th
-              className="p-3 text-sm font-semibold tracking-wide text-left"
-              key={index}
-            >
-              {colum}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>{children}</tbody>
-    </table>
+    <div className="overflow-auto rounded-lg shadow">
+      <table className="my-5 w-full uppercase">
+        <thead className="bg-gray-50 border-b-2 border-gray-200">
+          <tr>
+            {colums.map((colum, index) => (
+              <th
+                className="p-3 text-sm font-semibold tracking-wide text-left"
+                key={index}
+              >
+                {colum}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-100">{children}</tbody>
+      </table>
+    </div>
   );
 };
 
