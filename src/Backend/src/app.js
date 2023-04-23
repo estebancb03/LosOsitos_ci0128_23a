1adc3b8b03791f5145d8dbe9b0a130c5b578d75e
CARLOS ANDRES QUESADA ESTRADA <carlos.quesadaestrada@ucr.ac.cr>
2023-04-23 14:00:55 -0600

    Se actualiza la carpeta de Backend para continuar con la implementacion del esqueleto de la aplicacion

--- src/Backend/src/app.js
+++ src/Backend/src/app.js
@@ -1,6 +1,13 @@
-import express from "express";
+import express, { json } from "express";
 import config from "./config";
 
 const app = express()
+
+// Settings
+app.set("port", config.port)
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 export default app
--- src/Backend/src/database/connection.js
+++ src/Backend/src/database/connection.js
@@ -1,13 +1,18 @@
 import sql from "mssql"
+import config from "../config"
 
-const dbSettings = {
-    user,
-    password,
-    server,
-    database
+export const dbSettings = {
+    user: config.dbUser,
+    password: config.dbPassword,
+    server: config.dbServer,
+    database: config.dbDatabase,
+    options: {
+        encrypt: true,
+        trustSeverCertificate: true,
+    }
 }
 
-const getConnection = async () => {
+export const getConnection = async () => {
     try {
         const pool = await sql.connect(dbSettings)
         return pool
@@ -15,3 +20,5 @@ const getConnection = async () => {
         console.error(error)
     }
 }
+
+export {sql}
