b82c8cfdb50f4455271d3a68ff97972ca6c5843b
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-23 11:41:17 -0600

    Se agregan rutas para acceder a la pagina de Availability

--- src/Frontend/src/App.jsx
+++ src/Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Availability from "./pages/Availability";
 import DefaultLayout from "./layouts/DefaultLayout";
 
 const App = () => {
@@ -14,11 +15,13 @@ const App = () => {
         </Route>
         //Private access operator routes
         <Route path="/operator">
-          <Route index />
+          <Route index element={<Home />} />
+          <Route path="availability" element={<Availability />} />
         </Route>
         //Private access admin routes
         <Route path="/admin">
-          <Route index />
+          <Route index element={<Home />} />
+          <Route path="availability" element={<Availability />} />
         </Route>
       </Routes>
     </BrowserRouter>
