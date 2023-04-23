94353d35f38235c1c1871019f0b999735806d7d0
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-22 21:14:59 -0600

    Se arregla redimensionamiento del sidebar en dispositivos pequeños

--- src/Frontend/src/components/SideBarMenu.jsx
+++ src/Frontend/src/components/SideBarMenu.jsx
@@ -36,7 +36,7 @@ const SideBarMenu = () => {
           )
       }
       <div
-        className={`bg-[#21295c] top-0 right-0 fixed w-96 h-full p-5 ${
+        className={`bg-[#21295c] top-0 right-0 fixed w-96 sm:w-60 h-full p-5 ${
           sideBar ? "translate-x-0" : "translate-x-full"
         } ease-in-out duration-300`}
       >
