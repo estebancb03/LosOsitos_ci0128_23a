c253b246e0ed060cb9e7495eabb77221277df55d
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-23 12:21:13 -0600

    Se le agregan estilos al header de la tabla

--- src/Frontend/src/components/Table.jsx
+++ src/Frontend/src/components/Table.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
-const Table = () => {
+const Table = ({ colums, children }) => {
   return (
-    <div>Table</div>
-  )
-}
+    <table className="my-5 w-full">
+      <thead className="bg-gray-50 border-b-2 border-gray-200">
+        <tr>
+          {colums.map((colum, index) => (
+            <th
+              className="p-3 text-sm font-semibold tracking-wide text-left"
+              key={index}
+            >
+              {colum}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>{children}</tbody>
+    </table>
+  );
+};
 
-export default Table
+export default Table;
