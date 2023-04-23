4730308088d11316801438632dada280df0870f6
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-23 11:46:56 -0600

    Se crea esqueleto de la página

--- src/Frontend/src/components/Title.jsx
+++ src/Frontend/src/components/Title.jsx
@@ -2,7 +2,7 @@ const Title = ({ name }) => {
   return (
     <div>
       <span className="ml-1 text-3xl sm:text-2xl font-semibold">{name}</span>
-      <div className="h-1 w-full bg-[#21295c] rounded-md"></div>
+      <div className="mt-2 h-1 w-full bg-[#21295c] rounded-md"></div>
     </div>
   );
 };
--- src/Frontend/src/pages/Availability.jsx
+++ src/Frontend/src/pages/Availability.jsx
@@ -1,5 +1,16 @@
+import Title from "../components/Title";
+import Container from "../components/Container";
+import Footer from "../components/Footer/Footer";
+import NavMenu from "../components/NavMenu/NavMenu";
+
 const Availability = () => {
-  return <div>Availability</div>;
+  return <div>
+    <NavMenu />
+    <Container>
+      <Title name="Availability"></Title>
+    </Container>
+    <Footer />
+  </div>;
 };
 
 export default Availability;
