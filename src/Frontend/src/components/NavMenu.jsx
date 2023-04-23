0ad3e0d3e4171f8a8c58469e769c61d3653b05a5
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-22 21:09:34 -0600

    Se elimina redundancia de clases de Tailwind en opciones de menu

--- src/Frontend/src/components/NavMenu.jsx
+++ src/Frontend/src/components/NavMenu.jsx
@@ -11,18 +11,18 @@ const NavMenu = () => {
                 Img
             </Link>
         </div>
-        <ul className="text-gray-300 flex items-center w-full pl-20 opacity-100 opacity-0 top-[-400px] 
+        <ul className="text-gray-300 flex items-center w-full pl-20 opacity-100 top-[-400px] 
         transition-all ease-in duration-500 sm:hidden">
-            <Link className="mx-4 my-6 my-0 text-xl hover:text-gray-300 duration-500">
+            <Link className="mx-4 my-6 text-xl hover:text-gray-300 duration-500">
               Option
             </Link>
-            <Link className="mx-4 my-6 my-0 text-xl hover:text-gray-300 duration-500">
+            <Link className="mx-4 my-6 text-xl hover:text-gray-300 duration-500">
               Option
             </Link>
-            <Link className="mx-4 my-6 my-0 text-xl hover:text-gray-300 duration-500">
+            <Link className="mx-4 my-6 text-xl hover:text-gray-300 duration-500">
               Option
             </Link>
-            <Link className="mx-4 my-6 my-0 text-xl hover:text-gray-300 duration-500">
+            <Link className="mx-4 my-6  text-xl hover:text-gray-300 duration-500">
               Option
             </Link>
         </ul>
