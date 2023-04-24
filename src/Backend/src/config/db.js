5ffa1da8ecc81e7bf3700763b4ba5af32dff2a0e
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-24 02:51:33 -0600

    Estandarizacion con Prettier

--- src/Backend/src/config/db.js
+++ src/Backend/src/config/db.js
@@ -1,23 +1,23 @@
-import sql from "mssql"
+import sql from "mssql";
 
 const dbSettings = {
-    user: DB_USER,
-    password: DB_PASSWORD,
-    server: DB_SERVER,
-    database: DB_NAME,
-    options: {
-        encrypt: true,
-        trustSeverCertificate: true,
-    }
-}
+  user: DB_USER,
+  password: DB_PASSWORD,
+  server: DB_SERVER,
+  database: DB_NAME,
+  options: {
+    encrypt: true,
+    trustSeverCertificate: true,
+  },
+};
 
 const getConnection = async () => {
-    try {
-        const pool = await sql.connect(dbSettings)
-        return pool
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    const pool = await sql.connect(dbSettings);
+    return pool;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export default getConnection;
