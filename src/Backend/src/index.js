e23cbcc1b040380ca1bbe6c87d56a38e32f1b708
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-24 02:43:35 -0600

    Se corrige index.js

--- src/Backend/src/index.js
+++ src/Backend/src/index.js
@@ -1,2 +1,18 @@
-import app from "./app"
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+const corsOptions = {};
+const allowedDomains = [process.env.FRONTEND_URL];
+
+app.use(express.json());
+app.use(cors(corsOptions));
+//Routes
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
