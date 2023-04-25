f134b246f2c09c67dbd9928b0fc43ddd5caa6642
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-25 12:30:26 -0600

    Mapeo de datos externos en el reservation list

--- src/Frontend/src/pages/ReservationList.jsx
+++ src/Frontend/src/pages/ReservationList.jsx
@@ -1,43 +1,130 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Title from "../components/Title";
+import Modal from "../components/Modal";
 import Button from "../components/Button";
 import Table from "../components/Table/Table";
 import Container from "../components/Container";
 import Footer from "../components/Footer/Footer";
-import StatusText from "../components/StatusText";
 import NavMenu from "../components/NavMenu/NavMenu";
 import TableItem from "../components/Table/TableItem";
-import ReservationListModal from "../components/ReservationListModal";
 
-import AvailabilityTestData from "../data/AvailabilityTestData";
+import AvailabilityTestData from "../data/ReservationTestData";
 
 const ReservationList = () => {
-  const [modal, setModal] = useState(true);
-  const [modalInformation, setModalInformation] = useState({});
+  const [viewModal, setViewModal] = useState(false);
+  const [addCarModal, setAddCarModal] = useState(false);
+  const [information, setInformation] = useState({});
+
+  const tableColums = [
+    "Id",
+    "Customer",
+    "Reservation date",
+    "Start date",
+    "End date",
+    "Products",
+    "Pay",
+    "",
+    "",
+  ];
+
+  const setModalDataStatus = (itemID) => {
+    const itemSelected = AvailabilityTestData.filter(
+      (item) => item.reservationId == itemID
+    );
+    setInformation(itemSelected[0]);
+    setViewModal(true);
+  };
+
+  const extractProductNames = (products) => {
+    const result = products.map((product) => product.name);
+    return result;
+  };
 
   return (
     <>
       <NavMenu />
       <Container>
-        <ReservationListModal
-          state={modal}
-          setState={setModal}
+        <Modal
+          state={viewModal}
+          setState={setViewModal}
           title="Reservation Data"
-        ></ReservationListModal>
-        <Title name="Reservation List" />
-        <Table
-          colums={[
-            "Id",
-            "Customer",
-            "Reservation date",
-            "Start date",
-            "End date",
-            "Products",
-            "Pay",
-            "",
-          ]}
         >
+          <div className="my-3 grid grid-cols-1">
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">Reservation id:</span> {information.reservationId}
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">Id:</span> {information.id}
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">Name:</span>{" "}
+              {information.customer}
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">Nationality:</span>{" "}
+              {information.nationality}
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">Reservation date:</span>{" "}
+              {information.reservationDate}
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">Start date:</span>{" "}
+              {information.startDate}
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">End date:</span>{" "}
+              {information.endDate}
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">
+                People({information.peopleQuantity}):
+              </span>
+              <ul className="mb-2 list-disc">
+                {information.peopleType &&
+                  information.peopleType.map((person, index) => (
+                    <li key={index} className="mb-2 ml-10">
+                      {person.costarican ? "National" : "Foreign"},{" "}
+                      {person.adult ? "Adult" : "Child"}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">Product:</span>
+              <ul className="mb-2 list-disc">
+                {information.products &&
+                  information.products.map((product, index) => (
+                    <li key={index} className="mb-2 ml-10">
+                      {product.name} (${product.price})
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            <div className="mb-2 text-lg">
+              <span className="font-semibold">Car plates:</span>
+              <ul className="mb-2 list-disc">
+                {information.plateNumbers &&
+                  information.plateNumbers.map((plateNumber, index) => (
+                    <li key={index} className="mb-2 ml-10">
+                      {plateNumber}
+                    </li>
+                  ))}
+              </ul>
+            </div>
+            <div className="mb-2 text-lg font-semibold">
+              Total: ${information.totalPrice}
+            </div>
+          </div>
+        </Modal>
+        <Modal
+          state={addCarModal}
+          setState={setAddCarModal}
+          title="Add Plate Number"
+        ></Modal>
+        <Title name="Reservation List" />
+        <Table colums={tableColums}>
           {AvailabilityTestData.map((record, index) => (
             <TableItem
               key={index}
@@ -48,9 +135,14 @@ const ReservationList = () => {
                 record.reservationDate,
                 record.startDate,
                 record.endDate,
-                record.products,
-                record.pay,
-                <Button text="View" onclickFunction={(e) => setModal(true)} />,
+                extractProductNames(record.products),
+                record.totalPrice,
+                <Button
+                  text="View"
+                  onclickFunction={(e) =>
+                    setModalDataStatus(record.reservationId)
+                  }
+                />,
               ]}
             />
           ))}
