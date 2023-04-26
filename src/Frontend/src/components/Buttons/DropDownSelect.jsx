c512f82ae20cbae7477df393c7ca2b71a3373d25
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-26 01:29:14 -0600

    Se actualiza margenes del dropdown

--- src/Frontend/src/components/Buttons/DropDownSelect.jsx
+++ src/Frontend/src/components/Buttons/DropDownSelect.jsx
@@ -11,7 +11,7 @@ const DropDownSelect = ({ text, disabled, options }) => {
           <div className="mt-2">
             <select
               disabled={disabled}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
             >
               {options.map((option, index) => (
                 <option key={index}>{option}</option>
@@ -21,10 +21,10 @@ const DropDownSelect = ({ text, disabled, options }) => {
         </div>
       ) : (
         <div className="sm:col-span-3">
-          <div className="my-2 mx-1">
+          <div className="mt-3.5 mx-2">
             <select
               disabled={disabled}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
             >
               {options.map((option, index) => (
                 <option key={index}>{option}</option>
