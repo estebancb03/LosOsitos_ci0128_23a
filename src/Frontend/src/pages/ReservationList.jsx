5f559061e189426a054c46cbe5066dcfcaea0879
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-26 01:29:28 -0600

    Se actualiza arreglo con datos de prueba

--- src/Frontend/src/pages/ReservationList.jsx
+++ src/Frontend/src/pages/ReservationList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
 
 import Title from "../components/Title";
 import Modal from "../components/Modal";
@@ -6,6 +7,7 @@ import Button from "../components/Buttons/Button";
 import Table from "../components/Table/Table";
 import Container from "../components/Container";
 import Footer from "../components/Footer/Footer";
+import DropDownSelect from "../components/Buttons/DropDownSelect";
 import InputButton from "../components/Buttons/InputButton";
 import NavMenu from "../components/NavMenu/NavMenu";
 import TableItem from "../components/Table/TableItem";
@@ -14,8 +16,9 @@ import AvailabilityTestData from "../data/ReservationTestData";
 
 const ReservationList = () => {
   const [viewModal, setViewModal] = useState(false);
-  const [addCarModal, setAddCarModal] = useState(false);
   const [information, setInformation] = useState({});
+  const [modifyButton, setModifyButton] = useState("Modify");
+  const [disabledButtons, setDisabledButtons] = useState(true);
 
   const tableColums = [
     "Id",
@@ -23,11 +26,17 @@ const ReservationList = () => {
     "Reservation date",
     "Start date",
     "End date",
-    "Products",
+    "Services",
     "Pay",
     "",
     "",
   ];
+
+  const restartModal = () => {
+    setViewModal(false);
+    setDisabledButtons(true);
+    setModifyButton("Modify");
+  };
 
   const setModalDataStatus = (itemID) => {
     const itemSelected = AvailabilityTestData.filter(
@@ -37,8 +46,8 @@ const ReservationList = () => {
     setViewModal(true);
   };
 
-  const extractProductNames = (products) => {
-    const result = products.map((product) => product.name);
+  const extractProductNames = (services) => {
+    const result = services.map((product) => product.name);
     return result;
   };
 
@@ -48,86 +57,131 @@ const ReservationList = () => {
       <Container>
         <Modal
           state={viewModal}
-          setState={setViewModal}
+          setState={restartModal}
           title="Reservation Data"
         >
-          <InputButton text="Reservation ID" placeholderText={information.reservationId} disabled={true}/>
-          <InputButton text="Costumer ID" placeholderText={information.id} disabled={true}/>
-          <InputButton text="Name" placeholderText={information.customer} disabled={true}/>
-          <InputButton text="Nationality" placeholderText={information.nationality} disabled={true}/>
-        {/* <div className="my-3 grid grid-cols-1">
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">Reservation id:</span> {information.reservationId}
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">Id:</span> {information.id}
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">Name:</span>{" "}
-              {information.customer}
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">Nationality:</span>{" "}
-              {information.nationality}
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">Reservation date:</span>{" "}
-              {information.reservationDate}
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">Start date:</span>{" "}
-              {information.startDate}
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">End date:</span>{" "}
-              {information.endDate}
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">
-                People({information.peopleQuantity}):
-              </span>
-              <ul className="mb-2 list-disc">
-                {information.peopleType &&
-                  information.peopleType.map((person, index) => (
-                    <li key={index} className="mb-2 ml-10">
-                      {person.costarican ? "National" : "Foreign"},{" "}
-                      {person.adult ? "Adult" : "Child"}
-                    </li>
-                  ))}
-              </ul>
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">Product:</span>
-              <ul className="mb-2 list-disc">
-                {information.products &&
-                  information.products.map((product, index) => (
-                    <li key={index} className="mb-2 ml-10">
-                      {product.name} (${product.price})
-                    </li>
-                  ))}
-              </ul>
-            </div>
-            <div className="mb-2 text-lg">
-              <span className="font-semibold">Car plates:</span>
-              <ul className="mb-2 list-disc">
-                {information.plateNumbers &&
-                  information.plateNumbers.map((plateNumber, index) => (
-                    <li key={index} className="mb-2 ml-10">
-                      {plateNumber}
-                    </li>
-                  ))}
-              </ul>
-            </div>
-            <div className="mb-2 text-lg font-semibold">
-              Total: ${information.totalPrice}
-            </div>
-          </div> */}
+          <div className="my-3">
+            {
+              <Button
+                text={modifyButton}
+                onclickFunction={(e) => {
+                  setDisabledButtons(!disabledButtons);
+                  modifyButton === "Modify"
+                    ? setModifyButton("Save changes")
+                    : setModifyButton("Modify");
+                }}
+              />
+            }
+          </div>
+          <div className="grid grid-cols-2 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <InputButton
+              text="Reservation ID"
+              placeholderText={information.reservationId}
+              disabled={true}
+            />
+            <InputButton
+              text="Costumer ID"
+              placeholderText={information.id}
+              disabled={disabledButtons}
+            />
+          </div>
+          <InputButton
+            text="Name"
+            placeholderText={information.customer}
+            disabled={disabledButtons}
+          />
+          <InputButton
+            text="Nationality"
+            placeholderText={information.nationality}
+            disabled={disabledButtons}
+          />
+          <InputButton
+            text="Reservation Date"
+            placeholderText={information.reservationDate}
+            disabled={disabledButtons}
+          />
+          <div className="grid grid-cols-2 gap-x-8 gap-y-6 sm:grid-cols-2 mb-2">
+            <InputButton
+              text="Start Date"
+              placeholderText={information.startDate}
+              disabled={disabledButtons}
+            />
+            <InputButton
+              text="End Date"
+              placeholderText={information.endDate}
+              disabled={disabledButtons}
+            />
+          </div>
+          <label className="block text-xl font-semibold leading-6 text-gray-900">
+            People ({information.peopleQuantity})
+          </label>
+
+          <div className="grid grid-cols-2 mt-2 mb-3">
+            {information.peopleType &&
+              information.peopleType.map((person, index) => (
+                <span key={index} className="mx-1">
+                  <DropDownSelect
+                    disabled={disabledButtons}
+                    options={[
+                      "Foreign, Adult",
+                      "Foreign, Child",
+                      "National, Adult",
+                      "National, Child",
+                      "Special Visitor",
+                    ]}
+                  />
+                </span>
+              ))}
+          </div>
+          <label className="block text-xl font-semibold leading-6 text-gray-900">
+            Services
+          </label>
+          {information.services &&
+            information.services.map((service, index) => (
+              <div key={index} className="flex">
+                <div className="bg-gray-100 w-full rounded-sm my-2">
+                <label className="block text-lg font-semibold ml-3 leading-6 mt-2 text-gray-900">
+                  {service.name}
+                </label>
+                <div className="grid grid-cols-2 gap-x-2 gap-y-6 sm:grid-cols-1 mb-2">
+                  <InputButton
+                    text=""
+                    placeholderText={service.date}
+                    disabled={disabledButtons}
+                  />
+                  <DropDownSelect
+                    disabled={disabledButtons}
+                    options={[
+                      service.hour,
+                      "10:30",
+                      "11:30",
+                      "12:30",
+                      "13:30",
+                    ]}
+                  />
+                </div>
+                </div>
+              </div>
+            ))}
+          <label className="block text-xl font-semibold leading-6 text-gray-900">
+            Plate Numbers
+          </label>
+          <div className="grid grid-cols-2 mb-3">
+            {information.services &&
+              information.plateNumbers.map((plateNumber, index) => (
+                <InputButton
+                  key={index}
+                  placeholderText={plateNumber}
+                  disabled={disabledButtons}
+                />
+              ))}
+          </div>
+          <InputButton
+            text="Total price"
+            placeholderText={information.totalPrice}
+            disabled={disabledButtons}
+          />
         </Modal>
-        <Modal
-          state={addCarModal}
-          setState={setAddCarModal}
-          title="Add Plate Number"
-        ></Modal>
         <Title name="Reservation List" />
         <Table colums={tableColums}>
           {AvailabilityTestData.map((record, index) => (
@@ -140,7 +194,7 @@ const ReservationList = () => {
                 record.reservationDate,
                 record.startDate,
                 record.endDate,
-                extractProductNames(record.products),
+                extractProductNames(record.services),
                 record.totalPrice,
                 <Button
                   text="View"
