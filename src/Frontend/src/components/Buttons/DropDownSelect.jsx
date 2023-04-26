80108237d8d28666763914ff7a1661d21ed205aa
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-25 21:52:55 -0600

    Se agregan validaciones a los botones para que se ajusten mejor cuando no tienen texto

--- src/Frontend/src/components/Buttons/DropDownSelect.jsx
+++ src/Frontend/src/components/Buttons/DropDownSelect.jsx
@@ -3,21 +3,36 @@ import { useState } from "react";
 const DropDownSelect = ({ text, disabled, options }) => {
   return (
     <>
-      <div className="sm:col-span-3">
-        <label className="block text-sm font-medium leading-6 text-gray-900">
-          {text}
-        </label>
-        <div className="mt-2">
-          <select
-            disabled={disabled}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-          >
-            {options.map((option, index) => (
-              <option key={index}>{option}</option>
-            ))}
-          </select>
+      {text ? (
+        <div className="sm:col-span-3">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            {text}
+          </label>
+          <div className="mt-2">
+            <select
+              disabled={disabled}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+            >
+              {options.map((option, index) => (
+                <option key={index}>{option}</option>
+              ))}
+            </select>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="sm:col-span-3">
+          <div className="my-2 mx-1">
+            <select
+              disabled={disabled}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+            >
+              {options.map((option, index) => (
+                <option key={index}>{option}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+      )}
     </>
   );
 };
--- src/Frontend/src/components/Buttons/InputButton.jsx
+++ src/Frontend/src/components/Buttons/InputButton.jsx
@@ -1,12 +1,8 @@
-const InputButton = ({
-  text,
-  placeholderText,
-  disabled,
-  onChangeFunction,
-}) => {
+const InputButton = ({ text, placeholderText, disabled, onChangeFunction }) => {
   return (
     <>
-      <div className="grid grid-cols-1 my-3">
+      {text ? (
+        <div className="grid grid-cols-1 my-3">
           <div className="sm:col-span-2">
             <label className="block text-xl font-semibold leading-6 text-gray-900">
               {text}
@@ -22,6 +18,21 @@ const InputButton = ({
             </div>
           </div>
         </div>
+      ) : (
+        <div className="grid grid-cols-1 my-1 mx-2">
+          <div className="sm:col-span-2">
+            <div className="mt-2.5">
+              <input
+                type="text"
+                placeholder={placeholderText}
+                disabled={disabled}
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
+                onChange={onChangeFunction}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
