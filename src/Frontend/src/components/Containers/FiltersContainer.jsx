43c59aa1bf947ae62565e03d44da9466b582d092
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-27 09:52:08 -0600

    Se agrega botón de aplicar al contenedor de filtros

--- src/Frontend/src/components/Containers/FiltersContainer.jsx
+++ src/Frontend/src/components/Containers/FiltersContainer.jsx
@@ -1,7 +1,7 @@
 import Title from "../Title";
 import Button from "../Buttons/Button";
 
-const FiltersContainer = ({ onclickFunction, children }) => {
+const FiltersContainer = ({ applyFunction, restartFunction, children }) => {
   return (
     <>
       <div className="mt-5 mb-2 rounded-sm ring-4 ring-[#21295c]">
@@ -12,8 +12,13 @@ const FiltersContainer = ({ onclickFunction, children }) => {
         <div className="grid grid-cols-4 sm:grid-cols-1 md:grid-cols-3 my-7 mx-7 md:my-6 md:mx-6 sm:my-5 sm:mx-5">
           {children}
         </div>
-        <div className="mx-7 mb-5">
-          <Button text="Restart filters" type="delete" onclickFunction={onclickFunction} />
+        <div className="mx-6 mb-5 grid grid-cols-2 sm:grid-cols-1 sm:mx-5">
+          <div className="mr-3 sm:mr-0 sm:mb-2">
+            <Button text="Apply" type="add" onclickFunction={applyFunction} />
+          </div>
+          <div className="ml-3 sm:ml-0 sm:mt-2">
+            <Button text="Restart" type="delete" onclickFunction={restartFunction} />
+          </div>
         </div>
         <div className="h-[0.5px]"></div>
       </div>
