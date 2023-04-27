07d5147a9f175a50621f98842bbf96e2038a8de3
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-26 19:40:56 -0600

    Se crea componente para guardar los filtros

--- src/Frontend/src/components/Containers/FiltersContainer.jsx
+++ src/Frontend/src/components/Containers/FiltersContainer.jsx
@@ -1,5 +1,20 @@
-const FiltersContainer = () => {
-  return <div>FiltersContainer</div>;
+import Title from "../Title";
+
+const FiltersContainer = ({ children }) => {
+  return (
+    <>
+      <div className="rounded-sm ring-4 ring-[#21295c]">
+        <div className="h-[25px]"></div>
+        <div className="grid grid-cols-1 mx-7 md:mx-6 sm:mx-5">
+            <Title name="Filters" />
+        </div>
+        <div className="grid grid-cols-4 sm:grid-cols-1 md:grid-cols-3 my-7 mx-7 md:my-6 md:mx-6 sm:my-5 sm:mx-5">
+          {children}
+        </div>
+        <div className="h-[0.5px]"></div>
+      </div>
+    </>
+  );
 };
 
 export default FiltersContainer;
