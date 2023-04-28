51aee77638aa800243616d39d2f6d31777697672
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-28 10:13:58 -0600

    Se agrega una propiedad tipo al inputbutton

--- src/Frontend/src/components/Buttons/InputButton.jsx
+++ src/Frontend/src/components/Buttons/InputButton.jsx
@@ -1,4 +1,4 @@
-const InputButton = ({ text, placeholderText, disabled, onChangeFunction }) => {
+const InputButton = ({ text, placeholderText, disabled, type, onChangeFunction }) => {
   return (
     <>
       {text ? (
@@ -13,7 +13,7 @@ const InputButton = ({ text, placeholderText, disabled, onChangeFunction }) => {
                 placeholder={placeholderText}
                 disabled={disabled}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
-                onChange={onChangeFunction}
+                onChange={e => onChangeFunction(type, e.target.value)}
               />
             </div>
           </div>
@@ -27,7 +27,7 @@ const InputButton = ({ text, placeholderText, disabled, onChangeFunction }) => {
                 placeholder={placeholderText}
                 disabled={disabled}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
-                onChange={onChangeFunction}
+                onChange={e => onChangeFunction(type, e.target.value)}
               />
             </div>
           </div>
