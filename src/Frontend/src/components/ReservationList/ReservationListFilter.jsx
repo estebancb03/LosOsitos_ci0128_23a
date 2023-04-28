75bde378a1b2c17c965996838036fe6e5021b26e
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-28 16:35:17 -0600

    Adaptacion de diseño ahora dos columnas en pantalla pequeñas

--- src/Frontend/src/components/ReservationList/ReservationListFilter.jsx
+++ src/Frontend/src/components/ReservationList/ReservationListFilter.jsx
@@ -44,7 +44,7 @@ const ReservationListFilter = ({ reservationData, setReservationRecords }) => {
       if (match) acc.push(curr);
       return acc;
     }, []);
-  }
+  };
 
   // Method that applys the filters
   const applyFilters = (filter) => {
@@ -73,14 +73,17 @@ const ReservationListFilter = ({ reservationData, setReservationRecords }) => {
 
     const intersectionTM = intersection(typeFilterResults, methodFilterResults);
     const intersectionTMS = intersection(intersectionTM, serviceFilterResults);
-    const intersectionTMSR = intersection(intersectionTMS, customerIdFilterResults);
+    const intersectionTMSR = intersection(
+      intersectionTMS,
+      customerIdFilterResults
+    );
 
     setReservationRecords(intersectionTMSR);
   };
   return (
     <>
       <FiltersContainer applyFunction={applyFilters}>
-        <span className="">
+        <span className="sm:mr-3">
           <DropDownSelect
             text="Type"
             disabled={false}
@@ -89,7 +92,7 @@ const ReservationListFilter = ({ reservationData, setReservationRecords }) => {
             onChangeFunction={changeFiltersState}
           />
         </span>
-        <span className="mx-4 sm:mt-4 sm:mx-0">
+        <span className="mx-4 sm:mx-0 sm:ml-3">
           <DropDownSelect
             text="Method"
             disabled={false}
@@ -98,7 +101,7 @@ const ReservationListFilter = ({ reservationData, setReservationRecords }) => {
             onChangeFunction={changeFiltersState}
           />
         </span>
-        <span className="">
+        <span className="sm:mt-6 sm:mr-3">
           <DropDownSelect
             text="Service"
             disabled={false}
@@ -107,15 +110,19 @@ const ReservationListFilter = ({ reservationData, setReservationRecords }) => {
             onChangeFunction={changeFiltersState}
           />
         </span>
-        <InputButton text="Start Date" />
-        <span className="mx-4 sm:mx-0">
+        <span className="sm:mt-2 sm:ml-3">
+          <InputButton text="Start Date" />
+        </span>
+        <span className="mx-4 sm:mx-0 sm:mr-3">
           <InputButton text="End Date" />
         </span>
-        <InputButton
-          type="customerId"
-          text="Costumer Id"
-          onChangeFunction={changeFiltersState}
-        />
+        <span className="sm:ml-3">
+          <InputButton
+            type="customerId"
+            text="Costumer Id"
+            onChangeFunction={changeFiltersState}
+          />
+        </span>
       </FiltersContainer>
     </>
   );
