354399e4d3391b2cb3925e200df4346d8221c6d6
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-29 00:16:09 -0600

    Correccion de bug con placeholder

--- src/Frontend/src/components/ReservationList/ReservationListFilter.jsx
+++ src/Frontend/src/components/ReservationList/ReservationListFilter.jsx
@@ -179,6 +179,7 @@ const ReservationListFilter = ({ reservationData, setReservationRecords }) => {
           <InputButton
             type="customerId"
             text="Costumer Id"
+            placeholderText=""
             onChangeFunction={changeFiltersState}
           />
         </span>
