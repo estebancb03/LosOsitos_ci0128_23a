fadec30002ab11b0682acc7fbc9b7c49ce79ae74
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-28 21:34:30 -0600

    Reajustes en el posicionamiento del datepicker

--- src/Frontend/src/components/Buttons/DatePickerButton.jsx
+++ src/Frontend/src/components/Buttons/DatePickerButton.jsx
@@ -23,7 +23,7 @@ const DatePickerButton = ({ text, typeClass, type, onChangeFunction }) => {
     <div className="grid grid-cols-1">
       <div className="sm:col-span-2">
         {typeClass && typeClass === "1" ? (
-          <div className="mt-3 sm:ml-3">
+          <div className="mt-4 sm:ml-3">
             <label className="block mb-2 text-xl font-semibold leading-6 text-gray-900">
               {text}
             </label>
@@ -34,7 +34,7 @@ const DatePickerButton = ({ text, typeClass, type, onChangeFunction }) => {
               }}
               selected={startDate}
               dateFormat="dd/MM/yyyy"
-              className="block w-[100%] md:w-[93%] sm:w-[110%] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
+              className="block w-[100%] md:w-[93%] sm:w-[105%] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
             />
           </div>
         ) : (
@@ -49,7 +49,7 @@ const DatePickerButton = ({ text, typeClass, type, onChangeFunction }) => {
               }}
               selected={startDate}
               dateFormat="dd/MM/yyyy"
-              className="block w-[96%] md:w-[93%] sm:w-[92%] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
+              className="block w-[97%] md:w-[93%] sm:w-[96%] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#6545e6] sm:text-sm sm:leading-6"
             />
           </div>
         )}
