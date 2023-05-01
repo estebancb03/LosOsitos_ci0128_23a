d9162812f83cbcd1685d46410e8fff01b14b207a
Daniel Lizano <dlizano@outlook.com>
2023-05-01 01:06:45 -0600

    primera parte del formulario

--- src/Frontend/src/components/Reservation/ReservationStep0.jsx
+++ src/Frontend/src/components/Reservation/ReservationStep0.jsx
@@ -1,26 +1,49 @@
 import { useState } from "react"
 import Button from "../Buttons/Button";
 
-const ReservationStep1 = () => {
-    const [reservationType, setReservationType] = useState("");
-    return <>
-        <div className="grid grid-cols-2">
-            <div className="h-20">
-                <div className="h-10 mr-5 my-5">
-                    <Button className="mx-5 mr-5" text="Camping" type="add" onclickFunction={() => setReservationType("camping")} />
-                </div>
-            </div>
-            <div className="h-20">
-                <div className="h-10 ml-5 my-5">
-                    <Button className="mx-5 mr-5" text="Picnic" type="add" onclickFunction={() => setReservationType("picnic")} />
-                </div>
-            </div>
-        </div>
-        <div>
-            {reservationType === "camping" && <p className="h-10 bg-slate-500">Hello Camping!</p>}
-            {reservationType === "picnic" && <p className="h-10 bg-slate-500">Hello Picnic!</p>}
-        </div>
-    </>
+
+function ReservationStep0() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input className="bg-blue-200"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        ></textarea>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
-export default ReservationStep1
+export default ReservationStep0;
+
--- src/Frontend/src/pages/Home.jsx
+++ src/Frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Footer from "../components/Footer/Footer";
 import NavMenu from "../components/NavMenu/NavMenu";
 import Container from "../components/Containers/Container";
+import ReservationStep0 from "../components/Reservation/ReservationStep0";
 
 const Home = () => {
   return (
     <>
       <NavMenu />
-      <Container></Container>
+      <Container>
+        <ReservationStep0 />
+      </Container>
       <Footer />
     </>
   );
