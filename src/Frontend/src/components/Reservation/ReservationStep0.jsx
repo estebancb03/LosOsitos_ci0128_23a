b33edde99bb5742ee9e8fd3ccd14b3344b9990d7
Daniel Lizano <dlizano@outlook.com>
2023-05-01 01:46:40 -0600

    esqueleto de front end

--- src/Frontend/src/components/Reservation/ReservationStep0.jsx
+++ src/Frontend/src/components/Reservation/ReservationStep0.jsx
@@ -3,43 +3,74 @@ import Button from "../Buttons/Button";
 
 
 function ReservationStep0() {
-  const [name, setName] = useState('');
+  const [firstName, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState('');
+  const [idNumber, setIDNumber] = useState('');
+  const [nationality, setNationality] = useState('');
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
   }
 
   return (
     <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="ID number">ID Number:</label>
+            <input className="bg-blue-200 my-2"
+                type = "number"
+                id="ID number"
+                value={idNumber}
+                onChange={(e) => setIDNumber(e.target.value)}
+            />
+        </div>
       <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
+        <label htmlFor="name">First name:</label>
+        <input className="bg-blue-200 my-2"
+          type="firstName"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
       <div>
-        <label htmlFor="email">Email:</label>
-        <input className="bg-blue-200"
-          type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
+        <label htmlFor="email">Last name:</label>
+        <input className="bg-blue-200 my-2"
+          type="lastName"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
         />
       </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-        ></textarea>
-      </div>
+        <div>
+            <label htmlFor="age">Age:</label>
+            <input className="bg-blue-200 my-2"
+              type="age"
+              id="age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+        </div>
+        <div>
+          <label htmlFor="nationality">Nationality:</label>
+          <input className="bg-blue-200 my-2"
+            type="nationality"
+            id="nationality"
+            value={nationality}
+            onChange={(e) => setNationality(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="Email">Email:</label>
+          <input className="bg-blue-200 my-2"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
       <button type="submit">Submit</button>
     </form>
   );
