481123a9178cd9d95e445bbe3b7bdd808b15fefd
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-04-30 22:31:27 -0600

    Se agrega ruta para acceder a los datos de la reserva

--- src/Backend/src/routes/reservationListRoutes.js
+++ src/Backend/src/routes/reservationListRoutes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getReservations } from "../models/reservationListModel.js";
 
 const router = Router();
+
+router.get("/reservation-list", getReservations);
 
 export default router;
