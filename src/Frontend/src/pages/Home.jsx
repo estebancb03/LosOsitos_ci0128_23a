bb85b26790ddd55dea53da2cea64e32ae54e6ba2
Dylan Tenorio Rojas <dylantr2001@gmail.com>
2023-05-06 12:49:57 -0600

    elimina import en home hacia ReservationStep6

--- src/Frontend/src/pages/Home.jsx
+++ src/Frontend/src/pages/Home.jsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 import Footer from "../components/Footer/Footer";
 import NavMenu from "../components/NavMenu/NavMenu";
 import Container from "../components/Containers/Container";
-import ReservationStep6 from "../components/Reservation/ReservationStep6Sinpe";
 
 const Home = () => {
   return (
     <>
       <NavMenu />
       <Container>
-        <ReservationStep6 />
       </Container>
       <Footer />
     </>
