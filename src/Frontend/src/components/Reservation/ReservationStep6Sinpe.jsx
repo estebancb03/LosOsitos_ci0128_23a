8168791d2ca2a0e5e0224cbd682e7229e2df44a3
Dylan Tenorio Rojas <dylantr2001@gmail.com>
2023-05-06 17:18:43 -0600

    bugfix img a base64

--- src/Frontend/src/components/Reservation/ReservationStep6Sinpe.jsx
+++ src/Frontend/src/components/Reservation/ReservationStep6Sinpe.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import Title from "../Title";
 // Import React FilePond
 import { FilePond, registerPlugin } from 'react-filepond';
@@ -14,13 +14,20 @@ registerPlugin(FilePondPluginFileEncode);
 // Import Checkbox
 import { Checkbox } from 'antd';
 
+const saveBase64 = (setFilesBase64, files, filesBase64) => {
+  if (files.length != 0){
+    setFilesBase64(files[0].getFileEncodeBase64String())
+    console.log(filesBase64)
+  }
+}
+
 const ReservationStep6Sinpe = () => {
   const [files, setFiles] = useState([])
   const [filesBase64, setFilesBase64] = useState()
   const [checkbox, setCheckbox] = useState(false)
-  if (files.length != 0){
-    setFilesBase64(files[0].getFileEncodeBase64String())
-}
+  useEffect(() => {
+    saveBase64(setFilesBase64, files, filesBase64)
+  });
   return (
     <>
         <Title name="Upload Payment Proof Picture" />
@@ -33,7 +40,7 @@ const ReservationStep6Sinpe = () => {
         labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
         />
         <Checkbox onChange={() => {setCheckbox(!checkbox)
-          console.log(checkbox)}}>Agree with <a href="./javi.pdf">terms and conditions</a></Checkbox>
+          console.log(checkbox)}}>Agree with <a href="./termsconditions.jpeg">terms and conditions</a></Checkbox>
     </>
   );
 };
