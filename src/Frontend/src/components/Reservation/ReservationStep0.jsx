a8cbc1a805231460ba37e8c1280b4a9945da7bcf
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-05-06 18:14:33 -0600

    Se integra step0 con el form principal

--- src/Frontend/src/components/Reservation/ReservationStep0.jsx
+++ src/Frontend/src/components/Reservation/ReservationStep0.jsx
@@ -1,26 +1,53 @@
-import { useState } from "react"
+import { useState } from "react";
 import Button from "../Buttons/Button";
 
-const ReservationStep0 = () => {
-    const [reservationType, setReservationType] = useState("");
-    return <>
-        <div className="grid grid-cols-2">
-            <div className="h-20">
-                <div className="h-10 mr-5 my-5">
-                    <Button className="mx-5 mr-5" text="Camping" type="add" onclickFunction={() => setReservationType("camping")} />
-                </div>
-            </div>
-            <div className="h-20">
-                <div className="h-10 ml-5 my-5">
-                    <Button className="mx-5 mr-5" text="Picnic" type="add" onclickFunction={() => setReservationType("picnic")} />
-                </div>
-            </div>
-        </div>
+const ReservationStep0 = ({
+  windows,
+  setWindows,
+  reservationData,
+  setReservationData,
+}) => {
+  // Method that updates the reservation data
+  const updateReservationData = (Reservation_Type) => {
+    const newReservationData = {...reservationData};
+    const newWindows = {...windows};
+    newReservationData.Reservation_Type = Reservation_Type;
+    newWindows.Step0 = false;
+    newWindows.Step1 = true;
+    setReservationData(newReservationData);
+    setWindows(newWindows);
+  };
+
+  return (
+    <>
+      {windows.Step0 && (
         <div>
-            {reservationType === "camping" && <p className="h-10 bg-slate-500">Hello Camping!</p>}
-            {reservationType === "picnic" && <p className="h-10 bg-slate-500">Hello Picnic!</p>}
+          <div className="grid grid-cols-2">
+            <div className="h-20">
+              <div className="h-10 mr-5 my-5">
+                <Button
+                  className="mx-5 mr-5"
+                  text="Camping"
+                  type="add"
+                  onclickFunction={() => updateReservationData(1) }
+                />
+              </div>
+            </div>
+            <div className="h-20">
+              <div className="h-10 ml-5 my-5">
+                <Button
+                  className="mx-5 mr-5"
+                  text="Picnic"
+                  type="add"
+                  onclickFunction={() => updateReservationData(0)}
+                />
+              </div>
+            </div>
+          </div>
         </div>
+      )}
     </>
-}
+  );
+};
 
 export default ReservationStep0;
--- src/Frontend/src/pages/Reservation.jsx
+++ src/Frontend/src/pages/Reservation.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Title from "../components/Title";
 import Footer from "../components/Footer/Footer";
 import NavMenu from "../components/NavMenu/NavMenu";
@@ -5,18 +6,38 @@ import Container from "../components/Containers/Container";
 import ReservationStep0 from "../components/Reservation/ReservationStep0";
 
 const Reservation = () => {
-  return <>
-    <NavMenu />
-    <Container>
-      <Title name="Reservation" />
-      <div className="mt-6 shadow-sm ring-4 ring-inset rounded-md ring-[#21295c] mx-96 sm:mx-6">
-        <Container>
-          <ReservationStep0 />
-        </Container>
-      </div>
-    </Container>
-    <Footer />
-  </>;
+  // State that controls the visibility of the windows
+  const [windows, setWindows] = useState({
+    Step0: true,
+    Step1: false,
+    Step2: false,
+    Step3: false,
+    Step4: false,
+    Step5: false,
+    Step6: false,
+    Step7: false,
+  });
+  // State that controls the reservation data
+  const [reservationData, setReservationData] = useState({});
+  return (
+    <>
+      <NavMenu />
+      <Container>
+        <Title name="Reservation" />
+        <div className="mt-6 shadow-sm ring-4 ring-inset rounded-md ring-[#21295c] mx-96 sm:mx-6">
+          <Container>
+            <ReservationStep0
+              windows={windows}
+              setWindows={setWindows}
+              reservationData={reservationData}
+              setReservationData={setReservationData}
+            />
+          </Container>
+        </div>
+      </Container>
+      <Footer />
+    </>
+  );
 };
 
 export default Reservation;
