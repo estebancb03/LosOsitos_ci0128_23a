cd9e8bb46993ac9deb772b0f93f706cbb5b15077
Daniel Lizano <dlizano@outlook.com>
2023-05-06 18:43:01 -0600

    cambios con iconos

--- src/Frontend/src/components/PaymentMethod/PaymentMethod.jsx
+++ src/Frontend/src/components/PaymentMethod/PaymentMethod.jsx
@@ -1,9 +1,24 @@
 import { useState } from "react"
 import Button from "../Buttons/Button";
+import * as FaIcons from "react-icons/fa";
+import * as IoIcons from "react-icons/io";
+import Title from "../Title";
 
+
+
+
+function SearchBar() {
+    return (
+      <div>
+        <input type="text" placeholder="Search" />
+        <FaSearch />
+      </div>
+    );
+  }
 const PaymentMethod = () => {
     const [paymentMethod, setPaymentMethod] = useState("");
     return <>
+        <Title name="Payment Method" />
         <div className="grid grid-cols-3">
             <div className="h-20">
                 <div className="h-10 mr-5 my-5">
@@ -12,7 +27,12 @@ const PaymentMethod = () => {
             </div>
             <div className="h-20">
                 <div className="h-10 ml-5 my-5">
-                    <Button className="mx-5 mr-5" text="PayPal" type="add" onclickFunction={() => setPaymentMethod("Sinpe")} />
+                    <Button className="mx-5 mr-5" text="Sinpe" type="add" onclickFunction={() => setPaymentMethod("Sinpe")} />
+                </div>
+            </div>
+            <div className="h-20">
+                <div className="h-10 ml-5 my-5">
+                    <Button className="mx-5 mr-5" text="Cash" type="add" onclickFunction={() => setPaymentMethod("Cash")} /> 
                 </div>
             </div>
         </div>
