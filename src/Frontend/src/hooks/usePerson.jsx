05d7dc3a4c98f839505c0f9cafad9aa763d225c0
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-05-28 18:48:15 -0600

    Se crean hooks relacionados con la creacion de la reserva

---
+++ src/Frontend/src/hooks/usePerson.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import AxiosClient from "../config/AxiosClient";
+
+const usePerson = () => {
+
+};
+
+export default usePerson;
