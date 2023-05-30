7f9c2237672b0a3153dd0779f63d52c5740acd45
Daniel Lizano <dlizano@outlook.com>
2023-05-30 14:57:43 -0600

    generacion de csv

--- src/Frontend/src/components/Reports/FinancialReport.jsx
+++ src/Frontend/src/components/Reports/FinancialReport.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, forceUpdate, useReducer, useMemo, useCallback, useRef } from "react";
+import { CSVLink } from 'react-csv';
 import Button from "../Buttons/Button";
 import DropDownSelect from "../Buttons/DropDownSelect";
 import InputButton from "../Buttons/InputButton";
@@ -11,11 +12,10 @@ import {
 } from "../../helpers/formatDate";
 
 
-
 const FinancialReportsPage = () => {
     const [financialData, setFinancialData] = useState([]);
     const [endDate, setEndDate] = useState(new Date());
-    const [reportType, setReportType] = useState("daily");
+    const [reportType, setReportType] = useState('daily');
     const [reportData, setReportData] = useState([]);
     const [reportDataTotal, setReportDataTotal] = useState(0);
     const [reportDataTotalCash, setReportDataTotalCash] = useState(0);
@@ -39,30 +39,55 @@ const FinancialReportsPage = () => {
       }
     };
   
+    const downloadCSV = () => {
+      const csvData = financialData.map((report) => ({
+        Date: report.date,
+        Revenue: report.revenue,
+        Expenses: report.expenses,
+        Profit: report.profit,
+      }));
+  
+      // Generate a unique filename with timestamp
+      const filename = `financial_data_${new Date().getTime()}.csv`;
+  
+      // Create a CSVLink component with the generated CSV data
+      const csvLink = (
+        <CSVLink data={csvData} filename={filename}>
+          Download CSV
+        </CSVLink>
+      );
+  
+      // Return the CSVLink component
+      return csvLink;
+    };
+  
     return (
       <div>
         <h1>Financial Reports</h1>
         {financialData.length > 0 ? (
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Revenue</th>
-                <th>Expenses</th>
-                <th>Profit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {financialData.map((report) => (
-                <tr key={report.id}>
-                  <td>{report.date}</td>
-                  <td>{report.revenue}</td>
-                  <td>{report.expenses}</td>
-                  <td>{report.profit}</td>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Revenue</th>
+                  <th>Expenses</th>
+                  <th>Profit</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {financialData.map((report) => (
+                  <tr key={report.id}>
+                    <td>{report.date}</td>
+                    <td>{report.revenue}</td>
+                    <td>{report.expenses}</td>
+                    <td>{report.profit}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            {downloadCSV()}
+          </div>
         ) : (
           <p>Loading financial data...</p>
         )}
