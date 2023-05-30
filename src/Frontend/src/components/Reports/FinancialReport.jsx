137e6e461c985908f290fab0d55fd9887acf4c14
Daniel Lizano <dlizano@outlook.com>
2023-05-30 14:53:18 -0600

    esqueleto de query y html

--- src/Frontend/src/components/Reports/FinancialReport.jsx
+++ src/Frontend/src/components/Reports/FinancialReport.jsx
@@ -22,3 +22,56 @@ function FinancialReport() {
     const [reportDataTotalCard, setReportDataTotalCard] = useState(0);
     const [reportDataTotalOnline, setReportDataTotalOnline] = useState(0);
 }
+
+const FinancialReportsPage = () => {
+    const [financialData, setFinancialData] = useState([]);
+  
+    useEffect(() => {
+      fetchFinancialData()
+        .then((data) => setFinancialData(data))
+        .catch((error) => console.error(error));
+    }, []);
+  
+    const fetchFinancialData = async () => {
+      try {
+        const response = await fetch('/api/mieo/Ayuda?isra');
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error fetching financial data:', error);
+        return [];
+      }
+    };
+  
+    return (
+      <div>
+        <h1>Financial Reports</h1>
+        {financialData.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Revenue</th>
+                <th>Expenses</th>
+                <th>Profit</th>
+              </tr>
+            </thead>
+            <tbody>
+              {financialData.map((report) => (
+                <tr key={report.id}>
+                  <td>{report.date}</td>
+                  <td>{report.revenue}</td>
+                  <td>{report.expenses}</td>
+                  <td>{report.profit}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Loading financial data...</p>
+        )}
+      </div>
+    );
+  };
+  
+  export default FinancialReportsPage;
