c6a9cb0bea194c0169ad58aff4edb9cf2307f8c6
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-05-30 11:26:51 -0600

    Se crean archivos para componentes para dividir codigo de ShowReservation

---
+++ src/Frontend/src/components/ReservationList/Show/ShowPerson.jsx
@@ -0,0 +1,22 @@
+import InputButton from "../Buttons/InputButton";
+import DropDownSelect from "../Buttons/DropDownSelect";
+import DatePickerButton from "../Buttons/DatePickerButton";
+
+const  ShowPerson = (props) => {
+  // Props
+  const {
+    disabledElements,
+    reservation,
+    setReservation
+  } = props;
+  
+  
+  
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default ShowPerson;
