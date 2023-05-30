cbaf47cd88dd20ff6a2e1c0f4ef5b59ccc4e6949
Daniel Lizano <dlizano@outlook.com>
2023-05-30 14:38:02 -0600

    creacion de archivos base para reportes

---
+++ src/Frontend/src/components/Reports/FinancialReport.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect, forceUpdate, useReducer } from "react";
+import Button from "../Buttons/Button";
+import DropDownSelect from "../Buttons/DropDownSelect";
+import InputButton from "../Buttons/InputButton";
+import DatePickerButton from "../Buttons/DatePickerButton";
+import axiosClient from "../../config/AxiosClient";
+import {
+  formatDateDTDDMMYYYY,
+  changeDateInISOFormat,
+  isDateAfterISO8601,
+} from "../../helpers/formatDate";
