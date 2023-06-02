12966f252b360f43d971e03c34ef951f11b1645d
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-02 09:48:10 -0600

    Se crea modelo y rutas para acceder a los precios de los spots

--- src/Backend/src/index.js
+++ src/Backend/src/index.js
@@ -18,6 +18,7 @@ import clientRoutes from "./routes/clientRoutes.js";
 import serviceRoutes from "./routes/serviceRoutes.js";
 import spotsCampingRoutes from "./routes/spotsCampingRoutes.js";
 import serviceReservationRoutes from "./routes/serviceReservationRoutes.js";
+import spotPriceRoutes from "./routes/spotPriceRoutes.js";
 
 const app = express();
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
@@ -48,6 +49,7 @@ app.use('/api', vehicleRoutes);
 app.use('/api', clientRoutes);
 app.use('/api', serviceRoutes);
 app.use('/api', spotsCampingRoutes);
+app.use('/api', spotPriceRoutes);
 app.use('/api', serviceReservationRoutes);
 
 //app.use('/api', reservationListRoutes);
---
+++ src/Backend/src/models/spotPriceModel.js
@@ -0,0 +1,16 @@
+import { getConnection } from "../config/db.js";
+
+const getSpotPrices = async (req, res) => {
+  try {
+    const pool = await getConnection();
+    const result = await pool.request().query(`SELECT * FROM Spot_Price`);
+    console.log(result);
+    res.status(200);
+    res.json(result.recordset);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+export { getSpotPrices };
