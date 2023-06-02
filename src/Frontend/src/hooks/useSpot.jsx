e0a3b3c6c7ca6db06900de3300b9af8c01755eb4
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-02 10:25:47 -0600

    Se obtinene en el hook los spots y los precios de los spots

--- src/Frontend/src/hooks/useSpot.jsx
+++ src/Frontend/src/hooks/useSpot.jsx
@@ -3,7 +3,38 @@ import { useState, useEffect } from "react";
 import AxiosClient from "../config/AxiosClient";
 
 const useSpot = () => {
+  // State that controls the spots
+  const [spots, setSpots] = useState([]);
+  // State that controls the prices
+  const [spotsPrices, setSpotsPrices] = useState([]);
 
+  // Method that gets the all the spots
+  const getSpots = async () => {
+    try {
+      const url = '/spots/getAllSpots';
+      const { data } = await AxiosClient.get(url);
+      await setSpots(data);
+    } catch (exception) {
+      console.error(exception);
+    }
+  };
+
+  // Method that gets the all the spots
+  const getSpotsPrices = async () => {
+    try {
+      const url = '/spot-prices';
+      const { data } = await AxiosClient.get(url);
+      await setSpotsPrices(data);
+      console.log(data);
+    } catch (exception) {
+      console.error(exception);
+    }
+  };
+
+  useEffect(() => getSpots, []);
+  useEffect(() => getSpotsPrices, []);
+
+  return { spots, spotsPrices };
 };
 
 export default useSpot;
