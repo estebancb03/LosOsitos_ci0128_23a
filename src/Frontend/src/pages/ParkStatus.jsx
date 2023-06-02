a34ebaaba037dc8314a80872ffcc3548042784fd
Dylan Tenorio Rojas <dylantr2001@gmail.com>
2023-06-01 18:25:20 -0600

    Pagina del estado del parque con iconos y formato completo

--- src/Frontend/src/pages/ParkStatus.jsx
+++ src/Frontend/src/pages/ParkStatus.jsx
@@ -3,6 +3,8 @@ import Footer from "../components/Footer/Footer";
 import NavMenu from "../components/NavMenu/NavMenu";
 import Container from "../components/Containers/Container";
 import * as BsIcons from "react-icons/bs";
+import * as GiIcons from "react-icons/gi";
+import * as RiIcons from "react-icons/ri";
 
 const ParkStatus = () => {
   return (
@@ -10,18 +12,34 @@ const ParkStatus = () => {
       <NavMenu />
       <Container>
         <Title name="Park Status" />
-        <div className="object-center flex gap-4 flex-wrap place-content-center my-2">
-          <div className="w-2/5 text-center border-2 border-[#21295c] rounded">
-            <BsIcons.BsCarFrontFill />
+        <div className="flex gap-4 flex-wrap place-content-center my-2">
+          <div className="w-2/5 text-center border-2 border-[#21295c] rounded py-2">
+            <span className="flex justify-center my-1">
+              <BsIcons.BsPersonFill size={50} color="#21295c"/>
+            </span> 
+            <p>People</p>
+            <p>24</p>
           </div>
-          <div className="w-2/5 text-center border-2 border-[#21295c] rounded">
-            <BsIcons.BsCarFrontFill />
+          <div className="w-2/5 text-center border-2 border-[#21295c] rounded py-2">
+            <span className="flex justify-center my-1">
+              <BsIcons.BsCarFrontFill size={50} color="#21295c"/>
+            </span> 
+            <p>Vehicles</p>
+            <p>24</p>
           </div>
-          <div className="w-2/5 text-center border-2 border-[#21295c] rounded">
-            <BsIcons.BsCarFrontFill />
+          <div className="w-2/5 text-center border-2 border-[#21295c] rounded py-2">
+            <span className="flex justify-center my-1">
+              <GiIcons.GiCampingTent size={50} color="#21295c"/>
+            </span> 
+            <p>Camping</p>
+            <p>24</p>
           </div>
-          <div className="w-2/5 text-center border-2 border-[#21295c] rounded">
-            <BsIcons.BsCarFrontFill />
+          <div className="w-2/5 text-center border-2 border-[#21295c] rounded py-2">
+            <span className="flex justify-center my-1">
+              <RiIcons.RiShoppingBasketFill size={50} color="#21295c"/>
+            </span> 
+            <p>Picnic</p>
+            <p>24</p>
           </div>
         </div>
       </Container>
