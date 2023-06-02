552e0fc9e75d9a28ade25b88b1de8479147dff9b
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-01 19:00:10 -0600

    Se agregan metodos del hook para manipular datos del ticket

--- src/Frontend/src/hooks/useTicket.jsx
+++ src/Frontend/src/hooks/useTicket.jsx
@@ -100,9 +100,80 @@ const useTicket = () => {
     Ticket.Price = filteredPrices[0].Price;
   }
 
+  // Method that extracts the data of a ticket
+  const extractTicketData = (ticketName, Reservation_Type) => {
+    const data = {
+      Age_Range: 0,
+      Demographic_Group: 0,
+      Reservation_Type,
+      Special: 0,
+      Price: 0
+    };
+    if (ticketName === "Foreign-Adult") {
+      data.Demographic_Group = 1;
+      data.Age_Range = 1;
+    } else if (ticketName === "Foreign-Children") {
+      data.Demographic_Group = 1;
+      data.Age_Range = 0;
+    } else if (ticketName === "National-Adult") {
+      data.Demographic_Group = 0;
+      data.Age_Range = 1;
+    } else if (ticketName === "National-Children") {
+      data.Demographic_Group = 0;
+      data.Age_Range = 0;
+    }
+    ticketPrices.map((ticket) => {
+      if (ticket.Age_Range === data.Age_Range &&
+          ticket.Demographic_Group === data.Demographic_Group &&
+          ticket.Reservation_Type === data.Reservation_Type &&
+          ticket.Special === data.Special
+      ) {
+        data.Price = ticket.Price;
+      }
+    });
+    return data;
+  };
+
+  const modifyNewTicket = (type, value, reservation) => {
+    const newReservation = {...reservation};
+    const newTickets = [...reservation.NewTickets];
+    if (type[0] === "name") {
+      if (value === "Foreign-Adult") {
+        newTickets[type[1]].Demographic_Group = 1;
+        newTickets[type[1]].Age_Range = 1;
+      } else if (value === "Foreign-Children") {
+        newTickets[type[1]].Demographic_Group = 1;
+        newTickets[type[1]].Age_Range = 0;
+      } else if (value === "National-Adult") {
+        newTickets[type[1]].Demographic_Group = 0;
+        newTickets[type[1]].Age_Range = 1;
+      } else if (value === "National-Children") {
+        newTickets[type[1]].Demographic_Group = 0;
+        newTickets[type[1]].Age_Range = 0;
+      }
+    } else if (type[0] === "amount") {
+      newTickets[type[1]].Amount = value;
+    } else if (type[0] === "special") {
+      newTickets[type[1]].Special = value;
+    } else if (type[0] === "price") {
+      newTickets[type[1]].Price = value;
+    }
+
+    if (newTickets[type[1]].Special === 0 || type[0] === "name") {
+      getPriceByARDGCurrency(
+        newTickets[type[1]].Age_Range,
+        newTickets[type[1]].Demographic_Group,
+        newReservation.Reservation_Type,
+        newTickets[type[1]]
+        );
+    }
+    newReservation.NewTickets = newTickets;
+    return newReservation;
+  };
+
   useEffect(() => formatTicketPrices, []);
 
-  return { ticketOptions, ticketPrices, formatTicket, modifyTicket };
+  return { ticketOptions, ticketPrices, formatTicket, modifyTicket, modifyNewTicket, extractTicketData };
 };
 
 export default useTicket;
