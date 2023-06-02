b276fe9310ba61f63504f4d23c886dde042afe15
CarlosQ27 <carlquesada27@gmail.com>
2023-06-01 22:42:46 -0600

    Se comienza con la implementacion de la parte de configuracion de tiquetes, agregandolo a la pagina de Settings

--- src/Frontend/src/pages/Settings.jsx
+++ src/Frontend/src/pages/Settings.jsx
@@ -11,11 +11,14 @@ const Settings = () => {
       <NavMenu />
       <Container>
         <Title name="Settings" />
-        <div className="mt-6 shadow-sm ring-4 ring-inset rounded-md ring-[#21295c] mx-96 sm:mx-6 md:mx-10"></div>
+        <div className="mt-6 shadow-sm ring-4 ring-inset rounded-md ring-[#21295c] mx-96 sm:mx-6 md:mx-10" />
         <span className="ml-1 text-2xl sm:text-2xl font-semibold">
           Capacity
         </span>
         <Capacity />
+        <div className=" mt-4 ml-1 text-2xl sm:text-2xl font-semibold">
+          Tickets
+        </div>
       </Container>
       <Footer />
     </>
