50c1455f67c2fb9b54d1d8737302610949da1a6f
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-02 09:42:07 -0600

    Se agrega metodo y ruta para acceder a los spots existentes en la base

--- src/Backend/src/models/spotsModels.js
+++ src/Backend/src/models/spotsModels.js
@@ -21,6 +21,20 @@ const getAvailableSpotsByDates = async (req, res) => {
   }
 };
 
+const getAllSpots = async (req, res) => {
+  try {
+    const pool = await getConnection();
+    const result = await pool.request().query(`SELECT * FROM Spot`);
+    console.log(result);
+    res.status(200);
+    res.json(result.recordset);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
 export { 
   getAvailableSpotsByDates,
+  getAllSpots
 };
