dea42ceb6c8cc4884fee7566be56e2d2f6abf03e
Daniel Lizano <dlizano@outlook.com>
2023-06-02 20:03:30 -0600

    modificacion de datos en excel y csv

--- src/Frontend/src/components/Reports/FinancialReport.jsx
+++ src/Frontend/src/components/Reports/FinancialReport.jsx
@@ -13,7 +13,11 @@ import DropDownSelect from "../Buttons/DropDownSelect";
 import InputButton from "../Buttons/InputButton";
 import DatePickerButton from "../Buttons/DatePickerButton";
 import AxiosClient from "../../config/AxiosClient";
-import XLSX from 'xlsx';
+import XLSX from "xlsx";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+import { saveAs } from "file-saver";
+import { stringify } from 'csv-stringify';
 import {
   formatDateDTDDMMYYYY,
   changeDateInISOFormat,
@@ -30,21 +34,71 @@ const fetchFinancialData = async () => {
   }
 };
 
-
+const CsvGenerator = ({ data }) => {
+  const generateCsv = () => {
+    // Convertir los datos en formato CSV
+    stringify(data, { header: true }, (err, output) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // Crear el archivo CSV y descargarlo
+      const csvData = new Blob([output], { type: 'text/csv' });
+      const csvUrl = URL.createObjectURL(csvData);
+      const tempLink = document.createElement('a');
+      tempLink.href = csvUrl;
+      tempLink.setAttribute('download', 'data.csv');
+      tempLink.click();
+    });
+  };
+};
 
 const workbook = XLSX.utils.book_new();
 const worksheet = XLSX.utils.json_to_sheet(data);
 XLSX.utils.book_append_sheet(workbook, worksheet, "ReporteAutogenerado");
 
 // Generate a buffer from the workbook
-const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
 
 // Create a Blob from the buffer
-const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+const blob = new Blob([excelBuffer], {
+  type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+});
 
 // Save the Blob as a file
-const fileName = 'data.xlsx';
+const fileName = "data.xlsx";
 saveAs(blob, fileName);
+
+const PdfGenerator = ({ data }) => {
+  const pdfRef = useRef();
+
+  const generatePDF = () => {
+    const input = pdfRef.current;
+
+    const pdf = new jsPDF();
+    pdf.addHTML(input, () => {
+      pdf.save("output.pdf");
+    });
+  };
+};
+
+const ganerateAndSaveFile = () => {
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  XLSX.utils.book_append_sheet(workbook, worksheet, "ReporteAutogenerado");
+
+  // Generate a buffer from the workbook
+  const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+
+  // Create a Blob from the buffer
+  const blob = new Blob([excelBuffer], {
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+
+  // Save the Blob as a file
+  const fileName = "data.xlsx";
+  saveAs(blob, fileName);
+};
 
 const downloadCSV = () => {
   const csvData = financialData.map((report) => ({
