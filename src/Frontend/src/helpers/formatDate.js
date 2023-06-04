6069fc6d26d92c18f2882851cc7b85266e38d0d8
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-04 15:09:19 -0600

    Se corrige bug de seleccion de diferencia de dias

--- src/Frontend/src/helpers/formatDate.js
+++ src/Frontend/src/helpers/formatDate.js
@@ -113,11 +113,11 @@ const isDateAfterISO8601 = (date1, date2) => {
 
 // Method that found the day difference
 const getDaysDifference = (date1, date2) => {
-  const d1 = new Date(date1);
-  const d2 = new Date(date2);
+  const d1 = new Date(formatDateDTMMDDYYYY(date1));
+  const d2 = new Date(formatDateDTMMDDYYYY(date2));
   const diffInMs = Math.abs(d2 - d1);
   const diffInDays = Math.ceil(diffInMs / (1000 * 60 * 60 * 24));
-  return diffInDays;
+  return diffInDays + 1;
 }
 
 const formatDateYYYYMMDD = (date) => {
