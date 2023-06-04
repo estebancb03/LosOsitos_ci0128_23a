4b9806ef439a98104c85d80b0e159104bf8987a0
Israel Lopez <israel.lopez@ucr.ac.cr>
2023-06-03 20:21:11 -0600

    Se utiliza la página de reportes para obtener la información del reporte desde el backend.

--- src/Frontend/src/components/Reports/FinancialReport.jsx
+++ src/Frontend/src/components/Reports/FinancialReport.jsx
@@ -8,11 +8,6 @@ import {
   useRef,
 } from "react";
 import { CSVLink } from "react-csv";
-import Button from "../Buttons/Button";
-import DropDownSelect from "../Buttons/DropDownSelect";
-import InputButton from "../Buttons/InputButton";
-import DatePickerButton from "../Buttons/DatePickerButton";
-import AxiosClient from "../../config/AxiosClient";
 import XLSX from "xlsx";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
@@ -24,36 +19,24 @@ import {
   isDateAfterISO8601,
 } from "../../helpers/formatDate";
 
-const fetchFinancialData = async () => {
-  try {
-    const { data } = await AxiosClient.get("url mieo");
-    return data;
-  } catch (error) {
-    console.error("Error fetching financial data:", error);
-    return [];
-  }
+export const generateCsv = ({data}) => {
+  // Convertir los datos en formato CSV
+  stringify(data, { header: true }, (err, output) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    // Crear el archivo CSV y descargarlo
+    const csvData = new Blob([output], { type: 'text/csv' });
+    const csvUrl = URL.createObjectURL(csvData);
+    const tempLink = document.createElement('a');
+    tempLink.href = csvUrl;
+    tempLink.setAttribute('download', 'data.csv');
+    tempLink.click();
+  });
 };
 
-const CsvGenerator = ({ data }) => {
-  const generateCsv = () => {
-    // Convertir los datos en formato CSV
-    stringify(data, { header: true }, (err, output) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      // Crear el archivo CSV y descargarlo
-      const csvData = new Blob([output], { type: 'text/csv' });
-      const csvUrl = URL.createObjectURL(csvData);
-      const tempLink = document.createElement('a');
-      tempLink.href = csvUrl;
-      tempLink.setAttribute('download', 'data.csv');
-      tempLink.click();
-    });
-  };
-};
-
-const generateXlsx = () => {
+export const generateXlsx = () => {
   const workbook = XLSX.utils.book_new();
   const worksheet = XLSX.utils.json_to_sheet(data);
   XLSX.utils.book_append_sheet(workbook, worksheet, "ReporteAutogenerado");
@@ -71,91 +54,11 @@ const generateXlsx = () => {
   saveAs(blob, fileName);
 }
 
-const PdfGenerator = ({ data }) => {
+export const generatePdf = ({ data }) => {
   const pdfRef = useRef();
-
-  const generatePDF = () => {
-    const input = pdfRef.current;
-
-    const pdf = new jsPDF();
-    pdf.addHTML(input, () => {
-      pdf.save("output.pdf");
-    });
-  };
-};
-
-const ganerateAndSaveFile = () => {
-  const workbook = XLSX.utils.book_new();
-  const worksheet = XLSX.utils.json_to_sheet(data);
-  XLSX.utils.book_append_sheet(workbook, worksheet, "ReporteAutogenerado");
-
-  // Generate a buffer from the workbook
-  const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
-
-  // Create a Blob from the buffer
-  const blob = new Blob([excelBuffer], {
-    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  const input = pdfRef.current;
+  const pdf = new jsPDF();
+  pdf.addHTML(input, () => {
+    pdf.save("output.pdf");
   });
-
-  // Save the Blob as a file
-  const fileName = "data.xlsx";
-  saveAs(blob, fileName);
-};
-
-const downloadCSV = () => {
-  const csvData = financialData.map((report) => ({
-    Date: report.date,
-    Revenue: report.revenue,
-    Expenses: report.expenses,
-    Profit: report.profit,
-  }));
-
-  const FinancialReportsPage = () => {
-    const [financialData, setFinancialData] = useState([]);
-    const [endDate, setEndDate] = useState(new Date());
-    const [reportType, setReportType] = useState("daily");
-    const [reportData, setReportData] = useState([]);
-    const [reportDataTotal, setReportDataTotal] = useState(0);
-    const [reportDataTotalCash, setReportDataTotalCash] = useState(0);
-    const [reportDataTotalCard, setReportDataTotalCard] = useState(0);
-    const [reportDataTotalOnline, setReportDataTotalOnline] = useState(0);
-
-    useEffect(() => {
-      fetchFinancialData()
-        .then((data) => setFinancialData(data))
-        .catch((error) => console.error(error));
-    }, []);
-
-    const fetchFinancialData = async () => {
-      try {
-        const { data } = await AxiosClient.get("url mieo");
-        return data;
-      } catch (error) {
-        console.error("Error fetching financial data:", error);
-        return [];
-      }
-    };
-
-    const downloadCSV = () => {
-      const csvData = financialData.map((report) => ({
-        Date: report.date,
-        Revenue: report.revenue,
-        Expenses: report.expenses,
-        Profit: report.profit,
-      }));
-
-      // Generate a unique filename with timestamp
-      const filename = `financial_data_${new Date().getTime()}.csv`;
-
-      // Create a CSVLink component with the generated CSV data
-      const csvLink = (
-        <CSVLink data={csvData} filename={filename}>
-          Download CSV
-        </CSVLink>
-      );
-
-      // Return the CSVLink component
-      return csvLink;
-    };
-  };
 };
--- src/Frontend/src/pages/Reports.jsx
+++ src/Frontend/src/pages/Reports.jsx
@@ -5,23 +5,77 @@ import DatePickerButton from "../components/Buttons/DatePickerButton"
 import DropDownSelect from "../components/Buttons/DropDownSelect"
 import Button from "../components/Buttons/Button"
 
-const reportTypes = ["Visitors", "Income"]
+// import { generateCsv, generateXlsx, generatePdf } from "../components/Reports/FinancialReport"
+import { useState } from "react"
+import { getIncomeData, getVisitationData } from "../Queries"
+
+const reportTypes = ["Income", "Visitors"]
+const fileFormats = ["CSV", "Excel", "PDF"]
 
 const Reports = () => {
-    return (
-    <>
-        <NavMenu/>
-        <Container>
-            <Title name="Reports"/>
-            <div className="grid grid-cols-4 gap-4 my-5 sm:grid-cols-2">
-                <div><DropDownSelect text="Report type" options={reportTypes}/></div>
-                <div><DatePickerButton text="Start date"/></div>
-                <div><DatePickerButton text="End date"/></div>
-                <div className="mt-8"><Button text="Generate" type="add"/></div>
-            </div>
-        </Container>
-    </>
-    )
+  const [reportType, setReportType] = useState(reportTypes[0]);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [fileType, setFileType] = useState(fileFormats[0]);
+  const [reportData, setReportData] = useState([]);
+
+  const setValue = (type, value) => {
+    console.log(type, value);
+    if (type == "reportType") {
+      setReportType(value);
+    } else if (type == "startDate") {
+      setStartDate(value);
+    } else if (type == "endDate") {
+      setEndDate(value);
+    } else if (type == "fileType") {
+      setFileType(value);
+    }
+  }
+
+  const generateReport = async () => {
+    await getReportData();
+    formatReport(reportData);
+  }
+
+  const getReportData = async () => {
+    let result = []
+    try {
+      if (reportType == "Income") {
+        result = await getIncomeData(startDate, endDate);
+      } else if (reportType == "Visitors") {
+        result = await getVisitationData(startDate, endDate);
+      }
+      setReportData(result);
+    } catch (exception) {
+      console.error(exception);
+    }
+  }
+
+  const formatReport = () => {
+    // if (fileType == "CSV") {
+    //   generateCsv(reportData);
+    // } else if (fileType == "Excel") {
+    //   generateXlsx(reportData);
+    // } else if (fileType == "PDF") {
+    //   generatePdf(reportData);
+    // }
+  }
+
+  return (
+  <>
+    <NavMenu/>
+    <Container>
+      <Title name="Reports"/>
+      <div className="grid grid-cols-5 gap-4 my-5 sm:grid-cols-2">
+        <div><DropDownSelect text="Report type" typeChange="reportType" options={reportTypes} onChangeFunction={setValue}/></div>
+        <div><DatePickerButton text="Start date" type="startDate" onChangeFunction={setValue}/></div>
+        <div><DatePickerButton text="End date" type="endDate" onChangeFunction={setValue}/></div>
+        <div><DropDownSelect text="File type" typeChange="fileType" options={fileFormats} onChangeFunction={setValue}/></div>
+        <div className="mt-8"><Button text="Generate" type="add" onclickFunction={generateReport}/></div>
+      </div>
+    </Container>
+  </>
+  )
 }
 
 export default Reports;
