89cee684d4101c367c1f381b2cf9213aa0d3653a
CarlosQ27 <carlquesada27@gmail.com>
2023-06-03 22:49:41 -0600

    Se agrega validacion al componente de Tickets y mas modularizacion

--- src/Frontend/src/components/Settings/Tickets.jsx
+++ src/Frontend/src/components/Settings/Tickets.jsx
@@ -38,24 +38,34 @@ const Tickets = () => {
     true,
   ]);
   const [adultPicnicPrice, setAdultPicnicPrice] = useState(0);
-  const [childPicnicPrice, setchildPicnicPrice] = useState(0);
-  const [foreignAdultPicnicPrice, setforeignAdultPicnicPrice] = useState(0);
-  const [foreignChildPicnicPrice, setforeignChildPicnicPrice] = useState(0);
+  const [childPicnicPrice, setChildPicnicPrice] = useState(0);
+  const [foreignAdultPicnicPrice, setForeignAdultPicnicPrice] = useState(0);
+  const [foreignChildPicnicPrice, setForeignChildPicnicPrice] = useState(0);
   const [adultCampingPrice, setAdultCampingPrice] = useState(0);
   const [childCampingPrice, setChildCampingPrice] = useState(0);
   const [foreignAdultCampingPrice, setForeignAdultCampingPrice] = useState(0);
   const [foreignChildCampingPrice, setForeignChildCampingPrice] = useState(0);
+  const [isValidData, setIsValidData] = useState([
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+  ]);
 
-  const fecthTicketPrices = async () => {
+  const fetchTicketPrices = async () => {
     try {
       const result = await getTicketPrices();
-      filterTickets(result);
+      await filterTickets(result);
     } catch (exception) {
       console.error(exception);
     }
   };
 
-  const filterTickets = (result) => {
+  const filterTickets = async (result) => {
     setPicnicTickets(
       result.filter((ticket) => {
         return ticket.Reservation_Type === 0 && ticket.Special !== 1;
@@ -68,16 +78,183 @@ const Tickets = () => {
     );
   };
 
-  const readyToLoad = () => {
-    return picnicTickets.length > 0 && campingTickets.length > 0;
+  const checkValuesEntered = (index) => {
+    const regex = /^(?!.*[,])\d+(\.\d+)?$/;
+    let successfulConversion = true;
+    switch (index) {
+      case 0:
+        regex.test(childPicnicPrice)
+          ? setChildPicnicPrice(parseFloat(childPicnicPrice))
+          : (successfulConversion = false);
+        break;
+
+      case 1:
+        regex.test(foreignChildPicnicPrice)
+          ? setForeignChildPicnicPrice(parseFloat(foreignChildPicnicPrice))
+          : (successfulConversion = false);
+        break;
+
+      case 2:
+        regex.test(adultPicnicPrice)
+          ? setAdultPicnicPrice(parseFloat(adultPicnicPrice))
+          : (successfulConversion = false);
+        break;
+
+      case 3:
+        regex.test(adultPicnicPrice)
+          ? setForeignAdultPicnicPrice(parseFloat(foreignAdultPicnicPrice))
+          : (successfulConversion = false);
+        break;
+
+      case 4:
+        //console.log("Entre al case 4");
+        regex.test(childCampingPrice)
+          ? setChildCampingPrice(parseFloat(childCampingPrice))
+          : (successfulConversion = false);
+        break;
+
+      case 5:
+        //console.log("Entre al case 5");
+        regex.test(foreignChildCampingPrice)
+          ? setForeignChildCampingPrice(parseFloat(foreignChildCampingPrice))
+          : (successfulConversion = false);
+        break;
+
+      case 6:
+        //console.log("Entre al case 6");
+        regex.test(adultCampingPrice)
+          ? setAdultCampingPrice(parseFloat(adultCampingPrice))
+          : (successfulConversion = false);
+        break;
+
+      case 7:
+        //console.log("Entre al case 7");
+        regex.test(foreignAdultCampingPrice)
+          ? setForeignAdultCampingPrice(parseFloat(foreignAdultCampingPrice))
+          : (successfulConversion = false);
+        break;
+    }
+    return successfulConversion;
   };
 
-  useEffect(() => {
-    fecthTicketPrices();
-  }, []);
+  const assignValuesToStates = (index) => {
+    switch (index) {
+      case 0:
+        if (childPicnicPrice === 0) {
+          setChildPicnicPrice(picnicTickets[0].Price);
+        }
+        break;
 
-  const modifyPrice = () => {
-    console.log("Sirve??");
+      case 1:
+        if (foreignChildPicnicPrice === 0) {
+          setForeignChildPicnicPrice(picnicTickets[1].Price);
+        }
+        break;
+
+      case 2:
+        if (adultPicnicPrice === 0) {
+          setAdultPicnicPrice(picnicTickets[2].Price);
+        }
+        break;
+
+      case 3:
+        if (foreignAdultPicnicPrice === 0) {
+          setForeignAdultPicnicPrice(picnicTickets[3].Price);
+        }
+        break;
+
+      case 4:
+        if (childCampingPrice === 0) {
+          setChildCampingPrice(campingTickets[0].Price);
+        }
+        break;
+
+      case 5:
+        if (foreignChildPicnicPrice === 0) {
+          setForeignChildCampingPrice(campingTickets[1].Price);
+        }
+        break;
+
+      case 6:
+        if (adultCampingPrice === 0) {
+          setAdultCampingPrice(campingTickets[2].Price);
+        }
+        break;
+
+      case 7:
+        if (foreignAdultCampingPrice === 0) {
+          setForeignAdultCampingPrice(campingTickets[3].Price);
+        }
+        break;
+    }
+  };
+
+  const modifyHandleClick = (stateModified) => {
+    //console.log(
+    //  "Al modifyHandleClick le llego al stateModified: " + stateModified
+    //);
+    if (modifyButtons[stateModified] === "Save") {
+      if (checkValuesEntered(stateModified)) {
+        setIsValidData((prevValidData) => {
+          const updatedData = [...prevValidData];
+          updatedData[stateModified] = true;
+          return updatedData;
+        });
+      } else {
+        setIsValidData((prevValidData) => {
+          const updatedData = [...prevValidData];
+          updatedData[stateModified] = false;
+          return updatedData;
+        });
+      }
+    }
+    // else {
+    //   setIsValidData((prevValidData) => {
+    //     const updatedData = [...prevValidData];
+    //     updatedData[stateModified] = true;
+    //     return updatedData;
+    //   });
+    // }
+  };
+
+  const modifyPrice = (type, value) => {
+    //console.log("Reached modifyPrice !!!");
+    //console.log("Type[0] is " + type[0] + " y type[1] es " + type[1]);
+    const index = type[1];
+    switch (index) {
+      case 0:
+        //console.log("ModifyPrice case 0");
+        setChildPicnicPrice(value);
+        break;
+      case 1:
+        //console.log("ModifyPrice case 1");
+        setForeignChildPicnicPrice(value);
+        break;
+      case 2:
+        //console.log("ModifyPrice case 2");
+        setAdultPicnicPrice(value);
+        break;
+      case 3:
+        //console.log("ModifyPrice case 3");
+        setForeignAdultPicnicPrice(value);
+        break;
+      case 4:
+        //console.log("ModifyPrice case 4");
+        setChildCampingPrice(value);
+        break;
+      case 5:
+        //console.log("ModifyPrice case 5");
+        setForeignChildCampingPrice(value);
+        break;
+      case 6:
+        //console.log("ModifyPrice case 6");
+        setAdultCampingPrice(value);
+        break;
+      case 7:
+        //console.log("ModifyPrice case ");
+        setForeignAdultCampingPrice(value);
+        break;
+    }
   };
 
   const changeButtonAction = (index, type) => {
@@ -89,39 +266,132 @@ const Tickets = () => {
           return updatedButtons;
         });
       } else {
-        setModifyButtons((prevButtons) => {
-          const updatedButtons = [...prevButtons];
-          updatedButtons[index] = "Modify";
-          return updatedButtons;
-        });
+        if (isValidData[index]) {
+          setModifyButtons((prevButtons) => {
+            const updatedButtons = [...prevButtons];
+            updatedButtons[index] = "Modify";
+            return updatedButtons;
+          });
+          alert("Correctus");
+        } else {
+          alert(
+            "Values from the capacity can only be positive and intergers." +
+              "\nChanges will not be applied"
+          );
+        }
       }
     } else {
-      const offSet = index + 4;
-      console.log("En camping index + 4 es: " + offSet);
-      if (modifyButtons[offSet] === "Modify") {
+      const offSet = 4;
+      //console.log("En camping index + 4 es: " + offSet);
+      if (modifyButtons[index + offSet] === "Modify") {
         setModifyButtons((prevButtons) => {
           const updatedButtons = [...prevButtons];
-          updatedButtons[offSet] = "Save";
+          updatedButtons[index + offSet] = "Save";
           return updatedButtons;
         });
       } else {
-        setModifyButtons((prevButtons) => {
-          const updatedButtons = [...prevButtons];
-          updatedButtons[offSet] = "Modify";
-          return updatedButtons;
-        });
+        if (isValidData[index + offSet]) {
+          setModifyButtons((prevButtons) => {
+            const updatedButtons = [...prevButtons];
+            updatedButtons[index + offSet] = "Modify";
+            return updatedButtons;
+          });
+          alert("Correctus");
+        } else {
+          alert(
+            "Values from the capacity can only be positive and intergers." +
+              "\nChanges will not be applied"
+          );
+        }
       }
     }
   };
+
   const enableInput = (index, type) => {
-    type === "Picnic"
-      ? setDisabledPicnicButtons((prevButtons) =>
-          prevButtons.map((button, i) => (i === index ? !button : button))
-        )
-      : setDisabledCampingButtons((prevButtons) =>
+    if (type === "Picnic") {
+      if (isValidData[index]) {
+        setDisabledPicnicButtons((prevButtons) =>
           prevButtons.map((button, i) => (i === index ? !button : button))
         );
+      }
+    } else {
+      const offset = 4;
+      if (isValidData[index + offset]) {
+        setDisabledCampingButtons((prevButtons) =>
+          prevButtons.map((button, i) => (i === index ? !button : button))
+        );
+      }
+    }
+    // type === "Picnic"
+    //   ? setDisabledPicnicButtons((prevButtons) =>
+    //       prevButtons.map((button, i) => (i === index ? !button : button))
+    //     )
+    //   : setDisabledCampingButtons((prevButtons) =>
+    //       prevButtons.map((button, i) => (i === index ? !button : button))
+    //     );
   };
+
+  const returnState = (index) => {
+    assignValuesToStates(index);
+    switch (index) {
+      case 0:
+        return childPicnicPrice;
+      case 1:
+        return foreignChildPicnicPrice;
+      case 2:
+        return adultPicnicPrice;
+      case 3:
+        return foreignAdultPicnicPrice;
+      case 4:
+        return childCampingPrice;
+      case 5:
+        return foreignChildCampingPrice;
+      case 6:
+        return adultCampingPrice;
+      case 7:
+        return foreignAdultCampingPrice;
+    }
+  };
+
+  const readyToLoad = () => {
+    return picnicTickets.length > 0 && campingTickets.length > 0;
+  };
+
+  useEffect(() => {
+    fetchTicketPrices();
+  }, []);
+
+  useEffect(() => {
+    modifyHandleClick(0);
+  }, [childPicnicPrice]);
+
+  useEffect(() => {
+    modifyHandleClick(1);
+  }, [foreignChildPicnicPrice]);
+
+  useEffect(() => {
+    modifyHandleClick(2);
+  }, [adultPicnicPrice]);
+
+  useEffect(() => {
+    modifyHandleClick(3);
+  }, [foreignAdultPicnicPrice]);
+
+  useEffect(() => {
+    modifyHandleClick(4);
+  }, [childCampingPrice]);
+
+  useEffect(() => {
+    modifyHandleClick(5);
+  }, [foreignChildCampingPrice]);
+
+  useEffect(() => {
+    modifyHandleClick(6);
+  }, [adultCampingPrice]);
+
+  useEffect(() => {
+    modifyHandleClick(7);
+  }, [foreignAdultCampingPrice]);
 
   return (
     <>
@@ -137,7 +407,7 @@ const Tickets = () => {
                   rowNames[index],
                   picnicTicket.Currency,
                   <InputButton
-                    placeholderText={picnicTicket.Price.toLocaleString("en-us")}
+                    placeholderText={returnState(index).toLocaleString("en-us")}
                     disabled={disablePicnicButtons[index]}
                     type={["Picnic", index]}
                     onChangeFunction={modifyPrice}
@@ -163,11 +433,11 @@ const Tickets = () => {
                   rowNames[index],
                   campingTicket.Currency,
                   <InputButton
-                    placeholderText={campingTicket.Price.toLocaleString(
+                    placeholderText={returnState(index + 4).toLocaleString(
                       "en-us"
                     )}
                     disabled={disabledCampingButtons[index]}
-                    type={["Camping", index]}
+                    type={["Camping", index + 4]}
                     onChangeFunction={modifyPrice}
                   />,
                   <Button
