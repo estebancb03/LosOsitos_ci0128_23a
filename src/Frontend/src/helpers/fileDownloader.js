580d4212e57c9160d3e421c8655737c299518ac7
Israel Lopez <israel.lopez@ucr.ac.cr>
2023-06-04 01:35:30 -0600

    Se agrega un archivo dedicado a la descarga de reportes.

---
+++ src/Frontend/src/helpers/fileDownloader.js
@@ -0,0 +1,16 @@
+export const downloadCSV = (csvString, fileName) => {
+  const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8' });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = fileName;
+  link.style.display = 'none';
+
+  document.body.appendChild(link);
+  link.click();
+
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
+  
