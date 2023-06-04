71c9e07c0aeec444bf95b8153a89847bec50b005
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-04 17:58:01 -0600

    Tets unitario sobre modifyPersonData

--- src/Frontend/__test__/usePerson.test.js
+++ src/Frontend/__test__/usePerson.test.js
@@ -1,7 +1,7 @@
 import { renderHook } from "@testing-library/react-hooks";
 import usePerson from "../src/hooks/usePerson.jsx";
 
-it("Test that the person's information is modified correctly", () => {
+it("Test that the person's information is seted correctly", () => {
   const personData = [
     {
       Name: "John",
@@ -25,6 +25,33 @@ it("Test that the person's information is modified correctly", () => {
     LastName2: "Smith",
     Gender: 0,
     Email: "johndoe@gmail.com",
+    Country_Name: "Costa Rica",
+    State: "San José",
+    Birth_Date: "1990-01-01",
+  });
+});
+
+it("Test that the person's information is modified correctly", () => {
+  const reservation = {
+    ID: 1,
+    Name: "John",
+    LastName1: "Doe",
+    LastName2: "Smith",
+    Gender: 0,
+    Email: "johndoe@example.com",
+    Country_Name: "Costa Rica",
+    State: "San José",
+    Birth_Date: "1990-01-01",
+  };
+  const { modifyPersonData } = usePerson();
+  const result = modifyPersonData("gender", "Female", reservation);
+  expect(result).toEqual({
+    ID: 1,
+    Name: "John",
+    LastName1: "Doe",
+    LastName2: "Smith",
+    Gender: 1,
+    Email: "johndoe@example.com",
     Country_Name: "Costa Rica",
     State: "San José",
     Birth_Date: "1990-01-01",
