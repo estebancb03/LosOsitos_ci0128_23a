dc54acffb6d44264155025dd34e066cf36db9977
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-03 18:30:06 -0600

    Se agrega unit test para metodo de modificacion de persona

--- src/Frontend/__test__/usePerson.test.js
+++ src/Frontend/__test__/usePerson.test.js
@@ -6,7 +6,7 @@ it("test_set_person_data_correctly_sets_person_data_into_reservation", () => {
     {
       Name: "John",
       LastName1: "Doe",
-      LastName2: "Jones",
+      LastName2: "Smith",
       Gender: 0,
       Email: "johndoe@gmail.com",
       Country_Name: "Costa Rica",
@@ -22,9 +22,36 @@ it("test_set_person_data_correctly_sets_person_data_into_reservation", () => {
     ID: 1,
     Name: "John",
     LastName1: "Doe",
-    LastName2: "Jones",
+    LastName2: "Smith",
     Gender: 0,
     Email: "johndoe@gmail.com",
+    Country_Name: "Costa Rica",
+    State: "San José",
+    Birth_Date: "1990-01-01",
+  });
+});
+
+it("test_modify_person_data_correctly_modifies_reservation_data", () => {
+  const reservation = {
+    ID: 1,
+    Name: "John",
+    LastName1: "Doe",
+    LastName2: "Smith",
+    Gender: 0,
+    Email: "johndoe@example.com",
+    Country_Name: "Costa Rica",
+    State: "San José",
+    Birth_Date: "1990-01-01",
+  };
+  const { modifyPersonData } = usePerson();
+  const result = modifyPersonData("gender", "Female", reservation);
+  expect(result).toEqual({
+    ID: 1,
+    Name: "John",
+    LastName1: "Doe",
+    LastName2: "Smith",
+    Gender: 1,
+    Email: "johndoe@example.com",
     Country_Name: "Costa Rica",
     State: "San José",
     Birth_Date: "1990-01-01",
