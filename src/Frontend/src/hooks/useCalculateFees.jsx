e36b258b09c2875a3e42940fb0f861655fdbb95d
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-04 01:46:36 -0600

    Se crean metodos ezpejo para New spots y new services

--- src/Frontend/src/hooks/useCalculateFees.jsx
+++ src/Frontend/src/hooks/useCalculateFees.jsx
@@ -15,6 +15,19 @@ const useCalculateFees = (reservation) => {
     return fee;
   };
   
+  // Method that calculates the new spots fee
+  const calculateNewSpotsFee = () => {
+    let fee = 0;
+    const startDate = reservation.Start_Date;
+    const endDate = reservation.End_Date;
+    const days = getDaysDifference(startDate, endDate) + 1;
+    if (reservation.NewSpots) {
+      reservation.NewSpots.map((spot) => fee += spot.Price);
+    }
+    fee *= days;
+    return fee;
+  };
+
   // Method that calculates the services fee
   const calculateServicesFee = () => {
     let fee = 0;
@@ -25,9 +38,20 @@ const useCalculateFees = (reservation) => {
     return fee;
   };
 
+  // Method that calculastes the new services fee
+  const calculateNewServicesFee = () => {
+    let fee = 0;
+    if (reservation.NewServices) {
+      reservation.NewServices.map((service) => fee += service.Price * service.Quantity);
+    }
+    console.log(fee);
+    return fee;
+  };
+
   return {
     calculateSpotsFee,
-    calculateServicesFee
+    calculateServicesFee,
+    calculateNewServicesFee
   };
 };
 
