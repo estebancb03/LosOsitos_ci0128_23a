5eeeca8861640211a51ed9c123ef6c49d31ca7fc
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-04 01:42:57 -0600

    Se agrega metodo que calcula precio total de servicios existentes

--- src/Frontend/src/hooks/useCalculateFees.jsx
+++ src/Frontend/src/hooks/useCalculateFees.jsx
@@ -2,22 +2,32 @@ import { getDaysDifference } from "../helpers/formatDate";
 
 const useCalculateFees = (reservation) => {
   
+  // Method that calculates the spots fee
   const calculateSpotsFee = () => {
     let fee = 0;
     const startDate = reservation.Start_Date;
     const endDate = reservation.End_Date;
     const days = getDaysDifference(startDate, endDate) + 1;
     if (reservation.Spots) {
-      reservation.Spots.map((spot) => {
-        fee += spot.Price;
-      });
+      reservation.Spots.map((spot) => fee += spot.Price);
     }
     fee *= days;
     return fee;
   };
   
+  // Method that calculates the services fee
+  const calculateServicesFee = () => {
+    let fee = 0;
+    if (reservation.Services) {
+      reservation.Services.map((service) => fee += service.Price * service.Quantity);
+    }
+    console.log(fee);
+    return fee;
+  };
+
   return {
-    calculateSpotsFee
+    calculateSpotsFee,
+    calculateServicesFee
   };
 };
 
