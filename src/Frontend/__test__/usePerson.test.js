48ea3c21dbfe8190fc44a347bd424bb23b48a036
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-04 18:07:43 -0600

    Se agrega describe a las pruebas de usePerson

--- src/Frontend/__test__/usePerson.test.js
+++ src/Frontend/__test__/usePerson.test.js
@@ -1,9 +1,26 @@
 import { renderHook } from "@testing-library/react-hooks";
 import usePerson from "../src/hooks/usePerson.jsx";
 
-it("Test that the person's information is seted correctly", () => {
-  const personData = [
-    {
+describe('usePerson', () => {
+  test("setPersonData sets the new data into the reservation", () => {
+    const personData = [
+      {
+        Name: "John",
+        LastName1: "Doe",
+        LastName2: "Smith",
+        Gender: 0,
+        Email: "johndoe@gmail.com",
+        Country_Name: "Costa Rica",
+        State: "San José",
+        Birth_Date: "1990-01-01",
+      },
+      ];
+    const reservation = { ID: 1 };
+    const setReservation = jest.fn();
+    const { setPersonData } = usePerson();
+    setPersonData(personData, reservation, setReservation);
+    expect(setReservation).toHaveBeenCalledWith({
+      ID: 1,
       Name: "John",
       LastName1: "Doe",
       LastName2: "Smith",
@@ -12,48 +29,33 @@ it("Test that the person's information is seted correctly", () => {
       Country_Name: "Costa Rica",
       State: "San José",
       Birth_Date: "1990-01-01",
-    },
-  ];
-  const reservation = { ID: 1 };
-  const setReservation = jest.fn();
-  const { setPersonData } = usePerson();
-  setPersonData(personData, reservation, setReservation);
-  expect(setReservation).toHaveBeenCalledWith({
-    ID: 1,
-    Name: "John",
-    LastName1: "Doe",
-    LastName2: "Smith",
-    Gender: 0,
-    Email: "johndoe@gmail.com",
-    Country_Name: "Costa Rica",
-    State: "San José",
-    Birth_Date: "1990-01-01",
+    });
   });
-});
 
-it("Test that the person's information is modified correctly", () => {
-  const reservation = {
-    ID: 1,
-    Name: "John",
-    LastName1: "Doe",
-    LastName2: "Smith",
-    Gender: 0,
-    Email: "johndoe@example.com",
-    Country_Name: "Costa Rica",
-    State: "San José",
-    Birth_Date: "1990-01-01",
-  };
-  const { modifyPersonData } = usePerson();
-  const result = modifyPersonData("gender", "Female", reservation);
-  expect(result).toEqual({
-    ID: 1,
-    Name: "John",
-    LastName1: "Doe",
-    LastName2: "Smith",
-    Gender: 1,
-    Email: "johndoe@example.com",
-    Country_Name: "Costa Rica",
-    State: "San José",
-    Birth_Date: "1990-01-01",
+  test("modifyPersonDara changes the reservation object fields", () => {
+    const reservation = {
+      ID: 1,
+      Name: "John",
+      LastName1: "Doe",
+      LastName2: "Smith",
+      Gender: 0,
+      Email: "johndoe@example.com",
+      Country_Name: "Costa Rica",
+      State: "San José",
+      Birth_Date: "1990-01-01",
+    };
+    const { modifyPersonData } = usePerson();
+    const result = modifyPersonData("gender", "Female", reservation);
+    expect(result).toEqual({
+      ID: 1,
+      Name: "John",
+      LastName1: "Doe",
+      LastName2: "Smith",
+      Gender: 1,
+      Email: "johndoe@example.com",
+      Country_Name: "Costa Rica",
+      State: "San José",
+      Birth_Date: "1990-01-01",
+    });
   });
 });
