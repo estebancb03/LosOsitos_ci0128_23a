caee2d8e61808122a79bb69fe94fd97d30c184b6
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-04 23:48:00 -0600

    Se abrevia prueba sobre getAllServices

--- src/Backend/__test__/serviceModel.test.js
+++ src/Backend/__test__/serviceModel.test.js
@@ -2,27 +2,18 @@ import { getServicesOptions } from "../src/models/serviceModel.js";
 
 describe('getAllServices', () => {
   test('getAllServices returns the names of all services', async () => {
-    const mockPool = {
-      request: jest.fn().mockReturnThis(),
-      query: jest.fn().mockResolvedValue({
-        recordset: [
-          {Name: "Bicycle"},
-          {Name: "Hiking"},
-          {Name: "Kayak"},
-        ]
-      })
-    };
     const mockReq = {};
     const mockRes = {
       status: jest.fn(),
       json: jest.fn()
     };
-    await getServicesOptions(mockReq, mockRes, mockPool);
+    const expectedRecordset = [
+      { Name: "Bicycle" },
+      { Name: "Hiking" },
+      { Name: "Kayak" },
+    ];
+    await getServicesOptions(mockReq, mockRes);
     expect(mockRes.status).toHaveBeenCalledWith(200);
-    expect(mockRes.json).toHaveBeenCalledWith([
-      {Name: "Bicycle"},
-      {Name: "Hiking"},
-      {Name: "Kayak"},
-    ]);
+    expect(mockRes.json).toHaveBeenCalledWith(expectedRecordset);
   });
 });
