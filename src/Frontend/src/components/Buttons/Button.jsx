d44d518038ca9987ad93f74262c71de3b5d9f27e
Daniel Lizano <dlizano@outlook.com>
2023-06-13 15:47:02 -0600

    Update Button.jsx

--- src/Frontend/src/components/Buttons/Button.jsx
+++ src/Frontend/src/components/Buttons/Button.jsx
@@ -5,13 +5,13 @@ const Button = ({text, type, disabled = false, onclickFunction}) => {
         <button className="w-full h-full bg-green-700 uppercase tracking-wider rounded-sm hover:bg-green-600 opacity-100 top-[-400px] 
         transition-all ease-in duration-300" disabled={disabled} onClick={onclickFunction}>{text}</button>
       ) : type && type === "modify" ? (
-        <button className="w-full h-full bg-[#21295c] uppercase tracking-wider rounded-sm hover:bg-[#6545e6] opacity-100 top-[-400px] 
+        <button className="w-full h-full bg-[#004e98] uppercase tracking-wider rounded-sm hover:bg-[#758bfd] opacity-100 top-[-400px] 
         transition-all ease-in duration-300" disabled={disabled} onClick={onclickFunction}>{text}</button>
       ) : type && type === "delete" ? (
         <button className="w-full h-full bg-rose-700 uppercase tracking-wider rounded-sm hover:bg-rose-600 opacity-100 top-[-400px] 
         transition-all ease-in duration-300" disabled={disabled} onClick={onclickFunction}>{text}</button>
       ) : (
-        <button className="w-full h-full bg-[#21295c] uppercase tracking-wider rounded-sm hover:bg-[#6545e6] opacity-100 top-[-400px] 
+        <button className="w-full h-full bg-[#004e98] uppercase tracking-wider rounded-sm hover:bg-[#758bfd] opacity-100 top-[-400px] 
         transition-all ease-in duration-300" disabled={disabled} onClick={onclickFunction}>{text}</button>
       )}
     </div>
