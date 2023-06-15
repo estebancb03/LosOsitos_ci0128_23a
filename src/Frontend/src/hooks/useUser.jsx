5dc89b18a5d456dcd3d7ebf05ca5367d4321eec7
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-15 02:29:58 -0600

    Se añaden al hook de usuario metodos para crear y modificar un objeto usuario

--- src/Frontend/src/hooks/useUser.jsx
+++ src/Frontend/src/hooks/useUser.jsx
@@ -2,7 +2,40 @@ import React from "react";
 import AxiosClient from "../config/AxiosClient";
 
 const useUser = () => {
-  return {};
+  const createUser = () => {
+    return {
+      ID: "",
+      Name: "",
+      LastName1: "",
+      LastName2: "",
+      Gender: 0,
+      Email: "",
+      Country_Name: "Costa Rica",
+      Birth_Date: new Date().toISOString(),
+      State: "",
+      Username: "",
+      Password: "",
+      Type: 0
+    };
+  };
+
+  const modifyUserData = (type, value, user) => {
+    const newUser = {...user};
+    if (type === "usertype") {
+      if (value === "Admin") {
+        newUser.Type = 0;
+      } else {
+        newUser.Type = 1;
+      }
+    } else if (type === "username") {
+      newUser.Username = value;
+    } else if (type === "userpassword") {
+      newUser.Password = value;
+    }
+    return newUser;
+  };
+
+  return { createUser, modifyUserData };
 };
 
 export default useUser;
