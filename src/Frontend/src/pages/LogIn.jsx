73d1ffecd732c60cf1277ca499c831fd1309e1fc
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-17 16:59:40 -0600

    Se grea diseño del formulario de log in

--- src/Frontend/src/pages/LogIn.jsx
+++ src/Frontend/src/pages/LogIn.jsx
@@ -8,6 +8,7 @@ import NavMenu from "../components/NavMenu/NavMenu";
 import Container from "../components/Containers/Container";
 import InputButton from "../components/Buttons/InputButton";
 import PasswordButton from "../components/Buttons/PasswordButton";
+import img from "../assets/images/3-asojunquillal-logo.png";
 
 const LogIn = () => {
   
@@ -16,6 +17,39 @@ const LogIn = () => {
       <NavMenu />
       <Container>
         <Title name="LogIn"/>
+        <Container>
+          <div className="flex items-center justify-center">
+            <div className="flex h-full w-[50%] md:w-[100%] sm:w-[100%] ring-4 ring-[#004e98] rounded-lg drop-shadow-md">
+              <img className="float-left my-10 mx-10 h-64 w-64" src={img} />
+              <div className=" h-full w-full mr-10 my-10">
+                <div className="my-5 grid grid-cols-1">
+                  <InputButton
+                    text="Username"
+                    type="username"
+                    placeholderText=""
+                    disabled={false}
+                    onChangeFunction={() => {}}
+                  />
+                </div>
+                <div className="my-5 grid grid-cols-1">
+                  <PasswordButton
+                    text="Password"
+                    type="userpassword"
+                    placeholderText=""
+                    disabled={false}
+                    onChangeFunction={() => {}}
+                  />
+                </div>
+                <div className="my-5">
+                  <Button
+                    text="Log In"
+                    onclickFunction={() => {}}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Container>
       </Container>
       <Footer />
     </>
