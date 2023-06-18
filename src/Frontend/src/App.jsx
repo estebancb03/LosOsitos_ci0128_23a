b75128ff65906578acad82f6bac7540f4dbb33b2
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-18 16:35:29 -0600

    Se agrega provider de autenticacion a la App

--- src/Frontend/src/App.jsx
+++ src/Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthProvider from "./context/auth/authProvider";
 import Home from "./pages/Home";
 import ReservationList from "./pages/ReservationList";
 import DefaultLayout from "./layouts/DefaultLayout";
@@ -14,33 +15,35 @@ import "filepond/dist/filepond.min.css";
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        //Public access routes
-        <Route path="/" element={<DefaultLayout />}>
-          <Route index element={<Home />} />
-          <Route path="reservation" element={<Reservation />} />
-          <Route path="login" element={<LogIn />} />
-        </Route>
-        //Private access operator routes
-        <Route path="/operator">
-          <Route index element={<Home />} />
-          <Route path="reservation-list" element={<ReservationList />} />
-          <Route path="reservation" element={<Reservation />} />
-          <Route path="reports" element={<Reports />} />
-          <Route path="settings" element={<Settings/>} />
-          <Route path="park-status" element={<ParkStatus />} />
-        </Route>
-        //Private access admin routes
-        <Route path="/admin">
-          <Route index element={<Home />} />
-          <Route path="reservation-list" element={<ReservationList />} />
-          <Route path="reservation" element={<Reservation />} />
-          <Route path="reports" element={<Reports />} />
-          <Route path="settings" element={<Settings/>} />
-          <Route path="park-status" element={<ParkStatus />} />
-          <Route path="user-list" element={<UserList />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          //Public access routes
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<Home />} />
+            <Route path="reservation" element={<Reservation />} />
+            <Route path="login" element={<LogIn />} />
+          </Route>
+          //Private access operator routes
+          <Route path="/operator">
+            <Route index element={<Home />} />
+            <Route path="reservation-list" element={<ReservationList />} />
+            <Route path="reservation" element={<Reservation />} />
+            <Route path="reports" element={<Reports />} />
+            <Route path="settings" element={<Settings/>} />
+            <Route path="park-status" element={<ParkStatus />} />
+          </Route>
+          //Private access admin routes
+          <Route path="/admin">
+            <Route index element={<Home />} />
+            <Route path="reservation-list" element={<ReservationList />} />
+            <Route path="reservation" element={<Reservation />} />
+            <Route path="reports" element={<Reports />} />
+            <Route path="settings" element={<Settings/>} />
+            <Route path="park-status" element={<ParkStatus />} />
+            <Route path="user-list" element={<UserList />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
