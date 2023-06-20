3c52fa4ed339779d7672e303ebda171162d549d3
Daniel Lizano <dlizano@outlook.com>
2023-06-20 02:00:59 -0600

    cambios en botones y contenido

--- src/Frontend/src/pages/Home.jsx
+++ src/Frontend/src/pages/Home.jsx
@@ -15,16 +15,25 @@ const Home = () => {
         <div className="relative h-screen">
           <div className="absolute inset-0 flex items-center justify-center">
             <div className="w-full text-center text-white">
-              <h1 className="text-5xl font-bold">ASOJUNQUILLAL</h1>
-              <p className="text-3xl font-bold">Un retiro inolvidable</p>
-              <div className=" mx-20%">
-                <Button></Button>
+              <h1 className="text-6xl font-semi-bold mb-3">ASO JUNQUILLAL</h1>
+              <p className="text-3xl font-demi-bold mb-3">Un retiro inolvidable</p>
+              <p className="text-2xl font-demi-bold mb-2">
+                Contrary to popular belief, Lorem Ipsum is not simply random
+                text. It has roots in a piece of classical Latin literature from
+                45 BC, making it over 2000 years old
+              </p>
+              <div className="mx-20% flex justify-center space-x-10">
+                <button className="bg-white bg-opacity-25 text-black font-semi-bold py-4 px-8 rounded-full hover:bg-gray-300 transition-colors duration-300">
+                  Book now
+                </button>
+                <button className="bg-white bg-opacity-25 text-black font-semi-bold py-4 px-8 rounded-full hover:bg-gray-300 transition-colors duration-300">
+                  Contact us
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-
       <Footer />
     </>
   );
