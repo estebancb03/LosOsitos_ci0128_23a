33d0ef923db3b736598687312a9b1c7f74a5c988
Daniel Lizano <dlizano@outlook.com>
2023-06-20 02:48:44 -0600

    NUevas paginas

---
+++ src/Frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import Title from "../components/Title";
+import Footer from "../components/Footer/Footer";
+import NavMenu from "../components/NavMenu/NavMenu";
+import Container from "../components/Containers/Container";
