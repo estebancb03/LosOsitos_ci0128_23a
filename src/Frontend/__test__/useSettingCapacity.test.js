781bc4ffa5e8a36d9729ed8d594ae0a8ed1d0f84
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-19 19:43:23 -0600

    Simulación de autenticación para pruebas de useSettingCapacity

--- src/Frontend/__test__/useSettingCapacity.test.js
+++ src/Frontend/__test__/useSettingCapacity.test.js
@@ -1,9 +1,23 @@
 import { renderHook } from "@testing-library/react-hooks";
 import useSettingCapacity from "../src/hooks/useSettingCapacity";
 import { act } from "react-dom/test-utils";
+import { useContext } from "react";
+
+const mockAuthContext = {
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjEwNjAzMDg2MSAgICIsIlVzZXJuYW1lIjoiY2hpcXVpIiwiVHlwZSI6MCwiaWF0IjoxNjg3MjExNzIxLCJleHAiOjE2ODczNDg1MjF9.HnxyhiMF1fHgjZK88fRQXc7GoEeAlA8QbRC5irb905U",
+};
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useContext: jest.fn(),
+}));
 
 describe('useSettingCapacity', () => {
   test('capacityValues should not be empty', () => {
+    useContext.mockReturnValue(mockAuthContext);
+    const mockAxiosClient = jest.fn();
+    jest.mock("../src/config/AxiosClient", () => mockAxiosClient);
     const { result } = renderHook(() => useSettingCapacity());
     const { capacityValues } = result.current;
     expect(capacityValues).not.toBe([]);
@@ -11,6 +25,9 @@ describe('useSettingCapacity', () => {
 
     // Sets capacity values filtered
   test('setCapacityValues should not be empty', () => {
+    useContext.mockReturnValue(mockAuthContext);
+    const mockAxiosClient = jest.fn();
+    jest.mock("../src/config/AxiosClient", () => mockAxiosClient);
     const { result } = renderHook(() => useSettingCapacity());
     const { setCapacityValues } = result.current;
     act(() => {
