0a745b84d058d14ac5f270f47caf321e982057de
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-22 23:11:02 -0600

    Prueba unitaria para metodo que compara contraseñas con contraseñas hasheadas

--- src/Backend/__test__/encryption.test.js
+++ src/Backend/__test__/encryption.test.js
@@ -7,6 +7,13 @@ describe('encryption', () => {
     const encryptedPassword = await encrypt(password);
     expect(encryptedPassword).not.toEqual(password);
     expect(await bcrypt.compare(password, encryptedPassword)).toEqual(true);
+  });
+
+  test('Should return true if the value is equal to the hashed value', async () => {
+    const password = 'estebancb';
+    const encryptedPassword = '$2b$10$1akcJfv81aa5archkyezCe.Isz5LjFtC2Dln5.qjPGOkNuLvjoD3u';
+    const compareResult = await compare(password, encryptedPassword);
+    expect(compareResult).toEqual(true);
   });    
 });
 
