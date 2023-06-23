7292537831d9e21f83e1d019439295be352362fb
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-22 23:43:17 -0600

    Se agrega prueba que checkea la autorizacion de un token de operador

---
+++ src/Backend/__test__/authMiddleware.test.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { checkOperatorAuth, checkAdminAuth } from '../src/middlewares/authMiddleware.js';
+
+describe('authMiddleware', () => {
+  test('Should return an error message when token is invalid or expired', async () => {
+    const req = {
+      headers: {
+        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjExODA5MDM1OSAgICAgICIsIlVzZXJuYW1lIjoiZXN0ZWJhbmNiIiwiVHlwZSI6MCwiaWF0IjoxNjg3MjkwODE2LCJleHAiOjE2ODc0Mjc2MTZ9.dlhvW0Q1IiMQGom8QkSO_-0kIe2R0cfkBcNxRrIXRZk'
+      }
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const next = jest.fn();
+    await checkOperatorAuth(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(403);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Invalid token' });
+  });
+});
