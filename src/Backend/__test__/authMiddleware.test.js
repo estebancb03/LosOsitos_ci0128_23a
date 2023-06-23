124066c708afac1e3c2e939ca2298965be9d6216
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-22 23:44:10 -0600

    Se agrega prueba unitaria sobre método que checkea autorización de un token de administrador

--- src/Backend/__test__/authMiddleware.test.js
+++ src/Backend/__test__/authMiddleware.test.js
@@ -17,4 +17,20 @@ describe('authMiddleware', () => {
     expect(res.status).toHaveBeenCalledWith(403);
     expect(res.json).toHaveBeenCalledWith({ message: 'Invalid token' });
   });
+
+  test('Should return an error message when token is not an admin token', async () => {
+    const req = {
+      headers: {
+        authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjEwNjAzMDg2MSAgICAgICIsIlVzZXJuYW1lIjoiY2hpcXVpIiwiVHlwZSI6MSwiaWF0IjoxNjg3NDk4NTAxLCJleHAiOjE2ODc2MzUzMDF9.yXhv1da_qYIftuAzuEQ76RTPxbpfphLe2WMY5KjFgbM'
+      }
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const next = jest.fn();
+    await checkAdminAuth(req, res, next);
+    expect(res.status).toHaveBeenCalledWith(403);
+    expect(res.json).toHaveBeenCalledWith({ message: 'Invalid admin token' });
+  });
 });
