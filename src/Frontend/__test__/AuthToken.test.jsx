5570a2df078e596e28bfe908ca9ead64231657b2
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-22 23:57:08 -0600

    Se agrega test que valida que se añada Bearer al token

---
+++ src/Frontend/__test__/AuthToken.test.jsx
@@ -0,0 +1,11 @@
+import { renderHook } from "@testing-library/react-hooks";
+import AxiosClient from "../src/config/AxiosClient";
+import AuthToken from "../src/config/AuthToken";
+
+describe('AuthToken', () => {
+  test('Should add an Authorization header to AxiosClient', () => {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjExODA5MDM1OSAgICAgICIsIlVzZXJuYW1lIjoiZXN0ZWJhbmNiIiwiVHlwZSI6MCwiaWF0IjoxNjg3MjkwODE2LCJleHAiOjE2ODc0Mjc2MTZ9.dlhvW0Q1IiMQGom8QkSO_-0kIe2R0cfkBcNxRrIXRZk';
+    AuthToken(token);
+    expect(AxiosClient.defaults.headers.common['Authorization']).toBe(`Bearer ${token}`);
+  });
+});
