135400d4604fc44bda55ed92a030f3eada48b82a
Daniel Lizano <dlizano@outlook.com>
2023-06-23 02:07:41 -0600

    Nueva organizacion de informacion

--- src/Frontend/src/pages/ContactUs.jsx
+++ src/Frontend/src/pages/ContactUs.jsx
@@ -10,13 +10,20 @@ import DropDownSelect from "../components/Buttons/DropDownSelect";
 const ContactUs = () => {
   return (
     <>
-        <NavMenu />
+      <NavMenu />
+      <Container>
         <div className="bg-center bg-opacity-50 h-96  relative">
-            <img src={playaDev} alt="Playa Dev" className="h-full w-full object-cover bg-opacity-50" />
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold">
-              Contact us
-            </div>  
+          <img
+            src={playaDev}
+            alt="Playa Dev"
+            className="h-full w-full object-cover bg-opacity-50"
+          />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold">
+            Contact us
+          </div>
         </div>
+      </Container>
+      <Container>
         <div className="flex justify-start justify-items-start space-x-10">
           <div className="flex flex-col">
             <InputButton
@@ -43,18 +50,19 @@ const ContactUs = () => {
               disabled={false}
               type="Message"
               text="Message"
-              placeholderText=""  
+              placeholderText=""
             />
           </div>
-        </div>
-        <div className="flex justify-end items-center "> 
-          <div className="text-gray-700">
+          <div className="flex flex-col text-gray-700">
             <h1 className="font-bold text-4xl mb-4">Info</h1>
-            <p className="text-lg font-semi-bold">Phone number: +506 8906-0672</p>
+            <p className="text-lg font-semi-bold">
+              Phone number: +506 8906-0672
+            </p>
             <p className="text-lg font-semi-bold">Email: apatubaju@gmail.com</p>
           </div>
         </div>
-        <Footer />
+      </Container>
+      <Footer />
     </>
   );
 };
