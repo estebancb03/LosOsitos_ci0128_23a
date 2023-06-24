1c0e4875cc0e3550375b1b7c0a5067c5d1d8ba0e
Daniel Lizano <dlizano@outlook.com>
2023-06-24 00:27:31 -0600

    nueva pagina de informacion

--- src/Frontend/src/components/NavMenu/NavMenu.jsx
+++ src/Frontend/src/components/NavMenu/NavMenu.jsx
@@ -16,13 +16,13 @@ const NavMenu = () => {
           className="text-gray-300 flex items-center w-full pl-20 opacity-100 top-[-400px] 
           transition-all ease-in duration-500 sm:hidden"
         >
-          <Link className="mx-4 my-6 text-xl duration-500 hover:text-white">
+          <Link to="/" className="mx-4 my-6 text-xl duration-500 hover:text-white">
             Home
           </Link>
-          <Link className="mx-4 my-6 text-xl duration-500 hover:text-white">
+          <Link to="/information" className="mx-4 my-6 text-xl duration-500 hover:text-white">
             Information
           </Link>
-          <Link className="mx-4 my-6 text-xl duration-500 hover:text-white">
+          <Link to="/meet" className="mx-4 my-6 text-xl duration-500 hover:text-white">
             Meet Junquillal
           </Link>
         </ul>
