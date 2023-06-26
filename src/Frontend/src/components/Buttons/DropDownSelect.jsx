91075db7248cc804c2f63077cd4056869e8e4798
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-26 01:41:55 -0600

    Se crea DropDownSelect con Ant Design

--- src/Frontend/src/components/Buttons/DropDownSelect.jsx
+++ src/Frontend/src/components/Buttons/DropDownSelect.jsx
@@ -1,68 +1,51 @@
-import { useState } from "react";
+import { Select } from "antd";
 
-const DropDownSelect = ({
- text,
- disabled,
- datacy,
- selectedOption,
- options,
- typeChange,
- onChangeFunction,
-}) => {
- // State that manages the current value of the component
- const [currentOption, setCurrentOption] = useState(selectedOption);
- return (
-   <>
-     {text ? (
-       <div className="sm:col-span-3">
-         <label className="block text-xl font-semibold leading-6 text-gray-900">
-           {text}
-         </label>
-         <div className="mt-2">
-           <select
-             onChange={(e) => {
-               setCurrentOption(e.target.value);
-               onChangeFunction(typeChange, e.target.value);
-             }}
-             data-cy={datacy}
-             selected={currentOption}
-             value={currentOption}
-             disabled={disabled}
-             className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#004e98] sm:text-sm sm:leading-6 overflow-y-scroll"
-           >
-             {options.map((option, index) => (
-               <option key={index} value={option} className="overflow-y-scroll">
-                 {option}
-               </option>
-             ))}
-           </select>
-         </div>
-       </div>
-     ) : (
-       <div className="sm:col-span-3">
-         <div className="mt-3.5 mx-2">
-           <select
-             onChange={(e) => {
-               setCurrentOption(e.target.value);
-               onChangeFunction(typeChange, e.target.value);
-             }}
-             data-cy={datacy}
-             selected={currentOption}
-             value={currentOption}
-             disabled={disabled}
-             className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#004e98] sm:text-sm sm:leading-6 overflow-y-scroll"
-           >
-             {options.map((option, index) => (
-               <option key={index} value={option} className="overflow-y-scroll">
-                 {option}
-               </option>
-             ))}
-           </select>
-         </div>
-       </div>
-     )}
-   </>
- );
+const DropDownSelect = (props) => {
+  const { 
+    text,
+    disabled,
+    datacy,
+    selectedOption,
+    options,
+    typeChange,
+    onChangeFunction
+  } = props;
+
+  const handleChange = (value) => {
+    onChangeFunction(typeChange, value);
+  };
+
+  return (
+    <>
+      <div className="sm:col-span-3">
+        {text && (
+          <div>
+            <label className="block text-xl font-semibold leading-6 text-gray-900">
+              {text}
+            </label>
+            <div className="mt-2"></div>
+          </div>
+        )}
+        <Select 
+          data-cy={datacy}
+          className="w-full h-full text-xl" 
+          size="large"
+          disabled={disabled}
+          defaultValue={
+            selectedOption ? {label: selectedOption, value: selectedOption}  :
+            {label: options[0], value: options[0]}
+          }
+          options={options.map((option) => {
+            return {
+              label: option,
+              value: option
+            };
+          })}
+          onChange={handleChange} 
+        />
+      </div>
+    </>
+  );
 };
 
 export default DropDownSelect;
