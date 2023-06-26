373f954d189c2a6e748e84dd97c98bd483efc965
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-26 12:11:21 -0600

    Actualizacion del datepicker

--- src/Frontend/src/components/Buttons/DatePickerButton.jsx
+++ src/Frontend/src/components/Buttons/DatePickerButton.jsx
@@ -1,38 +1,45 @@
+import dayjs from 'dayjs';
+import moment from 'moment';
 import { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { formatDateMMDDYYYY } from "../../helpers/formatDate";
+import { DatePicker } from 'antd';
+import { formatDateMMDDYYYY, addZerosToDate } from "../../helpers/formatDate";
 
-const DatePickerButton = ({
-  datacy,
-  text,
-  typeClass,
-  type,
-  selectedDate,
-  disabled = false,
-  onChangeFunction,
-}) => {
-  // State that controls the date
-  const [startDate, setStartDate] = useState();
+const DatePickerButton = (props) => {
+  const {
+    datacy,
+    text,
+    typeClass,
+    type,
+    selectedDate,
+    disabled,
+    onChangeFunction
+  } = props;
+  const dateFormat = 'DD/MM/YYYY';
+  const [startDate, setStartDate] = useState(new Date());
+
+  const handleChange = (date) => {
+    setStartDate(date);
+    onChangeFunction(type, formatDateMMDDYYYY(new Date(date)));
+  };
 
   return (
-    <div className="grid grid-cols-1">
-      <div className="sm:col-span-2">
+    <>
+      <div className='grid grid-cols-1'>
+        <div className='sm:col-span-2'>
+
         {typeClass && typeClass === "1" ? (
           <div className="mt-4 sm:ml-3">
             <label className="block mb-2 text-xl font-semibold leading-6 text-gray-900">
               {text}
             </label>
             <DatePicker
-              onChange={(date) => {
-                setStartDate(date);
-                onChangeFunction(type, formatDateMMDDYYYY(date));
-              }}
-              data-cy={datacy}
+              data-cy={datacy} 
+              defaultValue={dayjs(startDate)} 
+              format={dateFormat}
               disabled={disabled}
-              selected={startDate}
-              dateFormat="dd/MM/yyyy"
-              className="block w-[100%] md:w-[100%] sm:w-[105%] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-[#004e98] sm:text-sm sm:leading-6"
+              size='large'
+              className='w-full'
+              onChange={handleChange}
             />
           </div>
         ) : typeClass && typeClass === "2" ? (
@@ -41,15 +48,13 @@ const DatePickerButton = ({
               {text}
             </label>
             <DatePicker
-              onChange={(date) => {
-                setStartDate(date);
-                onChangeFunction(type, formatDateMMDDYYYY(date));
-              }}
-              data-cy={datacy}
+              data-cy={datacy} 
+              defaultValue={dayjs(addZerosToDate(formatDateMMDDYYYY(selectedDate)), dateFormat)} 
+              format={dateFormat}
               disabled={disabled}
-              selected={selectedDate}
-              dateFormat="dd/MM/yyyy"
-              className="block w-[100%] md:w-[93%] sm:w-[105%] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-[#004e98] sm:text-sm sm:leading-6"
+              size='large'
+              className='w-full'
+              onChange={handleChange}
             />
           </div>
         ) : typeClass && typeClass === "3" ? (
@@ -58,15 +63,13 @@ const DatePickerButton = ({
               {text}
             </label>
             <DatePicker
-              onChange={(date) => {
-                setStartDate(date);
-                onChangeFunction(type, formatDateMMDDYYYY(date));
-              }}
-              data-cy={datacy}
-              selected={selectedDate}
+              data-cy={datacy} 
+              defaultValue={dayjs(addZerosToDate(formatDateMMDDYYYY(selectedDate)))} 
+              format={dateFormat}
               disabled={disabled}
-              dateFormat="dd/MM/yyyy"
-              className="block w-[100%] md:w-[93%] sm:w-[105%] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-[#004e98] sm:text-sm sm:leading-6"
+              size='large'
+              className='w-full'
+              onChange={handleChange}
             />
           </div>
         ) : (
@@ -75,20 +78,19 @@ const DatePickerButton = ({
               {text}
             </label>
             <DatePicker
-              onChange={(date) => {
-                setStartDate(date);
-                onChangeFunction(type, formatDateMMDDYYYY(date));
-              }}
-              data-cy={datacy}
+              data-cy={datacy} 
+              defaultValue={dayjs(startDate)} 
+              format={dateFormat}
               disabled={disabled}
-              selected={startDate}
-              dateFormat="dd/MM/yyyy"
-              className="block w-[100%] md:w-[100%] sm:w-[100%] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-[#004e98] sm:text-sm sm:leading-6"
+              size='large'
+              className='w-full'
+              onChange={handleChange}
             />
           </div>
         )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
