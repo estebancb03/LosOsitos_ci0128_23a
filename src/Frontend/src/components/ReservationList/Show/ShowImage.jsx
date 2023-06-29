17aec8ff9e24f35273bfd1f4ff1c057febd6b68d
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-29 09:50:31 -0600

    Cambios en asignación de tamaño a spots y espaciado de titulos

--- src/Frontend/src/components/ReservationList/Show/ShowImage.jsx
+++ src/Frontend/src/components/ReservationList/Show/ShowImage.jsx
@@ -13,7 +13,7 @@ const ShowImage = (props) => {
     <>
         {reservation.Payment_Proof != "null" &&
         <div className="mb-3 mt-6">
-            <label className="block text-xl font-semibold leading-6 text-gray-900">Payment Proof</label>
+            <label className="block text-xl font-semibold leading-6 text-gray-900 mb-3">Payment Proof</label>
             <div className="grid grid-cols-1">
                 <div className="my-1 mr-3">
                     <img src={reservation.Payment_Proof} className="h-auto max-w-full rounded border bg-white p-1 dark:border-neutral-700 dark:bg-neutral-800"
