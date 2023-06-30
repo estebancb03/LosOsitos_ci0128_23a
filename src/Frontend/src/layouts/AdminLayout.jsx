a35aed4ccb99cb1ef1c5fa9185af3b755842f9dc
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-29 19:12:27 -0600

    Se agrega funcionalidad de autologout

--- src/Frontend/src/layouts/AdminLayout.jsx
+++ src/Frontend/src/layouts/AdminLayout.jsx
@@ -1,18 +1,28 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import authContext from "../context/auth/authContext";
+import useAuth from "../hooks/useAuth";
 
 const AdminLayout = () => {
   const navigate = useNavigate();
   const AuthContext = useContext(authContext);
-  const { type } = AuthContext;
+  const { type, auth, token, expiredTokenCkeck } = AuthContext;
+  const { expiredSesion } = useAuth();
+
+  useEffect(() => {
+    const expired = expiredSesion(token);
+    if (expired) {
+      navigate('/login');
+    };
+  });
+
   return (
     <>
-      {type === 0 ? (
+      {type === 0 && (
         <main className="mx-auto bg-[#FDFFFC] font-sans">
           <Outlet />
         </main>
-        ) : navigate("/")}
+        )}
     </>
   );
 };
