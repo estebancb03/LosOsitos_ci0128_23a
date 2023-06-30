4907fb2b9d69a040144daffaadf4e189d205f582
Dylan Tenorio Rojas <dylantr2001@gmail.com>
2023-06-30 00:19:49 -0600

    se cambia el orden para que sea mas facil visualizar los datos

--- src/Frontend/src/pages/TestimonialsAdmin.jsx
+++ src/Frontend/src/pages/TestimonialsAdmin.jsx
@@ -54,21 +54,6 @@ const TestimonialsAdmin = () => {
       >
         <h1 className="text-gray-500 text-4xl font-bold mb-8">Testimonials</h1>
         <div className="bg-white p-8 rounded-lg w-1/2 mt-8">
-          <h2 className="text-2xl font-bold mb-4">Reviews</h2>
-          {reviews.length === 0 ? (
-            <p>No reviews yet.</p>
-          ) : (
-            reviews.map((review, index) => (
-              <div key={index} className="mb-4">
-                <h3 className="text-lg font-bold">{review.Name + " " +review.LastName1}</h3>
-                <h4 className="text-sm">{formatDateDTDDMMYYYY(review.Date)}</h4>
-                <h4 className="text-sm">State: {review.State === 1 ? "Approved" : "Rejected"}</h4>
-                <p className="text-gray-700">{review.Description}</p>
-              </div>
-            ))
-          )}
-        </div>
-        <div className="bg-white p-8 rounded-lg w-1/2 mt-8">
           <h2 className="text-2xl font-bold mb-4">Pending Reviews</h2>
           {pendingReviews.length === 0 ? (
             <p>No pending reviews.</p>
@@ -92,6 +77,21 @@ const TestimonialsAdmin = () => {
                     Deny
                   </button>
                 </div>
+              </div>
+            ))
+          )}
+        </div>
+                <div className="bg-white p-8 rounded-lg w-1/2 mt-8">
+          <h2 className="text-2xl font-bold mb-4">Reviews</h2>
+          {reviews.length === 0 ? (
+            <p>No reviews yet.</p>
+          ) : (
+            reviews.map((review, index) => (
+              <div key={index} className="mb-4">
+                <h3 className="text-lg font-bold">{review.Name + " " +review.LastName1}</h3>
+                <h4 className="text-sm">{formatDateDTDDMMYYYY(review.Date)}</h4>
+                <h4 className="text-sm">State: {review.State === 1 ? "Approved" : "Rejected"}</h4>
+                <p className="text-gray-700">{review.Description}</p>
               </div>
             ))
           )}
