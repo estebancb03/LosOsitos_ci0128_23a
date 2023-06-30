c5164f6886eebd3c56e0a6a903fbc8509d2b7172
Esteban Castañeda Blanco <esteban.castaneda@ucr.ac.cr>
2023-06-30 15:05:30 -0600

    Se modifca prueba de createUser para que cumpla completamente POM

--- src/Frontend/cypress/e2e/CreateUser.cy.js
+++ src/Frontend/cypress/e2e/CreateUser.cy.js
@@ -1,42 +1,30 @@
 /// <reference types='cypress' />
 
 import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Userlist from '../pages/Userlist';
-import SideBarMenu from '../pages/SideBarMenu';
 
 const home = new Home();
-const login = new Login();
-const userlist = new Userlist();
-const sideBarMenu = new SideBarMenu();
 
 describe('<UserList />', () => {
   it('Operator user registration process', () => {
     // Arrange
     home.visit();
-    login.visit();
     // Actions
-    login.insertUsername('estebancb');
-    login.insertPassword('estebancb');
-    login.submit();
-    cy.wait(1000);
-    sideBarMenu.open();
-    sideBarMenu.navigateToOption('User list');
-    userlist.createUser();
-    userlist.insertID('123456789');
-    userlist.insertName('Test');
-    userlist.insertLastName1('Test');
-    userlist.insertLastName2('Test');
-    userlist.insertEmail('test@gmail.com');
-    userlist.selectGender('Female');
-    userlist.selectCountry('Costa Rica');
-    userlist.selectState('Guanacaste');
-    userlist.selectRole('Operator');
-    userlist.insertUsername('test');
-    userlist.insertPassword('test1234')
-    userlist.saveUser();
-    cy.wait(1000);
+    const { adminHome } = home.login('estebancb', 'estebancb');
+    adminHome.sideBarMenu.navigateToOption('User list');
+    adminHome.userList.createUser({
+      id: '123456789',
+      name: 'Test',
+      lastname1: 'Test',
+      lastname2: 'Test',
+      email: 'test@gmail.com',
+      gender: 'Female',
+      country: 'Costa Rica',
+      state: 'Guanacaste',
+      role: 'Operator',
+      username: 'test',
+      password: 'test1234'
+    });
     // Asserts
-    userlist.verifyUserExist('test');
+    adminHome.userList.verifyUserExist('test');
   });
 });
--- src/Frontend/cypress/pages/AdminHome.js
+++ src/Frontend/cypress/pages/AdminHome.js
@@ -1,7 +1,10 @@
-import SideBarMenu from "./SideBarMenu";
+import Userlist from './Userlist'
+import SideBarMenu from './SideBarMenu';
+
 class AdminHome {
   constructor() {
     this.sideBarMenu = new SideBarMenu();
+    this.userList = new Userlist();
   }
 
 }``
