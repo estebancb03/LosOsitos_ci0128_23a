8da8e42aa07a4634df1fb578fd0677d2553b3021
Daniel Lizano <dlizano@outlook.com>
2023-07-04 01:09:10 -0600

    borramos lorem ipsum

--- src/Frontend/src/pages/HomePage.jsx
+++ src/Frontend/src/pages/HomePage.jsx
@@ -17,11 +17,9 @@ const HomePage = () => {
           <div className="absolute inset-0 flex items-center justify-center">
             <div className="w-full text-center text-white">
               <h1 className="text-6xl font-semi-bold mb-3">ASO JUNQUILLAL</h1>
-              <p className="text-3xl font-demi-bold mb-3">Un retiro inolvidable</p>
+              <p className="text-3xl font-demi-bold mb-3">An unforgetable experience </p>
               <p className="text-2xl font-demi-bold mb-2">
-                Contrary to popular belief, Lorem Ipsum is not simply random
-                text. It has roots in a piece of classical Latin literature from
-                45 BC, making it over 2000 years old
+                Welcome to the best place to relax and enjoy the nature
               </p>
               <div className="mx-60% flex justify-center space-x-10">
                 <Link to="/reservation" className="bg-white bg-opacity-50 text-black font-semi-bold py-4 px-12 rounded-full hover:bg-gray-300 transition-colors duration-300">
